2f874810cc2e2d34407b859e430746e7156e05b3
Robert Pincus <Robert.Pincus@colorado.edu>
2018-11-22 09:11:28 -0500

    Values of physical contants consistent with 2018 redefinition of SI units.

--- rrtmgp/mo_rrtmgp_constants.F90
+++ rrtmgp/mo_rrtmgp_constants.F90
@@ -23,18 +23,19 @@ module mo_rrtmgp_constants
   use mo_rte_kind, only: wp
 
   ! -----------------------------------------
-  ! Physical constants
+  ! Physical constants, 2018 SI defintion of metric system   -
+  !   (see https://en.wikipedia.org/wiki/2019_redefinition_of_SI_base_units)
   ! Boltzmann constant [J/K] = [(kg m^2)/(K s^2)]
-  real(wp), parameter :: k_boltz = 1.380650e-23_wp
+  real(wp), parameter :: k_boltz = 1.380649e-23_wp
 
   !  molecular weight of water [kg/mol]
   real(wp), parameter :: m_h2o =  0.018016_wp
 
   ! Avogadro's number [molec/mol]
-  real(wp), parameter :: avogad = 6.02214199e23_wp
+  real(wp), parameter :: avogad = 6.02214076e23_wp
 
   ! Universal gas constant [J/(mol K)]
-  real(wp), parameter :: R_univ_gconst = 8.314472_wp
+  real(wp), parameter :: R_univ_gconst = avogad * k_boltz
 
   ! -----------------------------------------
   !

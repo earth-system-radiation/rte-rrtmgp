3ccb2c4cee70a36906e1fb22e847932983cc6f09
Robert Pincus <Robert.Pincus@colorado.edu>
2019-03-26 15:31:44 -0400

    Data movement in LW two-stream

--- rte/kernels-openacc/mo_rte_solver_kernels.F90
+++ rte/kernels-openacc/mo_rte_solver_kernels.F90
@@ -451,7 +451,7 @@ contains
       !
       ! adding computes only diffuse flux; flux_dn is total
       !
-      !$acc  parallel loop collapse(3) 
+      !$acc  parallel loop collapse(3)
       do igpt = 1, ngpt
         do ilay = 1, nlay+1
           do icol = 1, ncol
@@ -610,11 +610,10 @@ contains
     real(wp), parameter :: LW_diff_sec = 1.66  ! 1./cos(diffusivity angle)
     ! ---------------------------------
     ! ---------------------------------
-    !$acc  parallel loop collapse(3) &
-    !$acc&     copyout(tdif(:ncol,:nlay,:ngpt)) &
-    !$acc&     copyin(w0(:ncol,:nlay,:ngpt),g(:ncol,:nlay,:ngpt)) &
-    !$acc&     copyout(gamma1(:ncol,:nlay,:ngpt),rdif(:ncol,:nlay,:ngpt),gamma2(:ncol,:nlay,:ngpt)) &
-    !$acc&     copyin(tau(:ncol,:nlay,:ngpt))
+    !$acc enter data copyin(tau, w0, g)
+    !$acc enter data create(gamma1, gamma2, Rdif, Tdif)
+
+    !$acc  parallel loop collapse(3)
     do igpt = 1, ngpt
       do ilay = 1, nlay
         do icol = 1, ncol
@@ -653,6 +652,8 @@ contains
         end do
       end do
     end do
+    !$acc exit data delete (tau, w0, g)
+    !$acc exit data copyout(gamma1, gamma2, Rdif, Tdif)
   end subroutine lw_two_stream
   ! -------------------------------------------------------------------------------------------------
   !
@@ -672,9 +673,10 @@ contains
     integer :: icol, ilay, igpt
     ! ---------------------------------------------------------------
     ! ---------------------------------
-    !$acc  parallel loop collapse(3)  &
-    !$acc&     copy(lev_source(:ncol,:nlay+1,:ngpt)) &
-    !$acc&     copyin(lev_src_inc(:ncol,0:nlay,:ngpt),lev_src_dec(:ncol,:nlay+1,:ngpt))
+    !$acc enter data copyin(lev_src_inc, lev_src_dec)
+    !$acc enter data create(lev_source)
+
+    !$acc  parallel loop collapse(3)
     do igpt = 1, ngpt
       do ilay = 1, nlay+1
         do icol = 1,ncol
@@ -689,6 +691,8 @@ contains
         end do
       end do
     end do
+    !$acc exit data delete (lev_src_inc, lev_src_dec)
+    !$acc exit data copyout(lev_source)
   end subroutine lw_combine_sources
   ! ---------------------------------------------------------------
   !
@@ -719,15 +723,10 @@ contains
     real(wp)            :: lev_source_bot, lev_source_top
     ! ---------------------------------------------------------------
     ! ---------------------------------
+    !$acc enter data copyin(sfc_emis, sfc_src, lay_source, tau, gamma1, gamma2, rdif, tdif, lev_source)
+    !$acc enter data create(source_dn, source_up, source_sfc)
 
-    !$acc parallel loop collapse(3) &
-    !$acc&     copyin(sfc_emis(:ncol,:ngpt),rdif(:ncol,:nlay,:ngpt)) &
-    !$acc&     copy(source_dn(:ncol,:nlay,:ngpt)) &
-    !$acc&     copyin(sfc_src(:ncol,:ngpt)) &
-    !$acc&     copy(source_sfc(:ncol,:ngpt)) &
-    !$acc&     copyin(tdif(:ncol,:nlay,:ngpt),tau(:ncol,:nlay,:ngpt),lev_source(:ncol,:nlay+1,ngpt)) &
-    !$acc&     copy(source_up(:ncol,:nlay,:ngpt)) &
-    !$acc&     copyin(gamma1(:ncol,:nlay,:ngpt),gamma2(:ncol,:nlay,:ngpt))
+    !$acc parallel loop collapse(3)
     do igpt = 1, ngpt
       do ilay = 1, nlay
         do icol = 1, ncol
@@ -757,6 +756,9 @@ contains
         end do
       end do
     end do
+    !$acc exit data delete(sfc_emis, sfc_src, lay_source, tau, gamma1, gamma2, rdif, tdif, lev_source)
+    !$acc exit data copyout(source_dn, source_up, source_sfc)
+
   end subroutine lw_source_2str
   ! -------------------------------------------------------------------------------------------------
   !

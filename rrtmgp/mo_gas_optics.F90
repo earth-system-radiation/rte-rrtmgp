ac822b0e0c52fc6d74b48b9d77b465f5d4bbd426
Benjamin Hillman <bhillma@sandia.gov>
2019-03-27 13:37:07 -0400

    Manage thismo_gas_optics.F90gpoint_flavor in compute_gas_taus

--- rrtmgp/mo_gas_optics.F90
+++ rrtmgp/mo_gas_optics.F90
@@ -475,7 +475,8 @@ contains
     !
     !$acc enter data create(tau, tau_rayleigh, jtemp, jpress, jeta, tropo, fmajor, fminor, col_mix)
     !$acc enter data copyin(play, tlay, col_gas)
-    !$acc enter data copyin(this%flavor, this%press_ref_log, this%temp_ref, this%vmr_ref)
+    !$acc enter data copyin(this%flavor, this%press_ref_log, this%temp_ref, this%vmr_ref, &
+    !$acc&                  this%gpoint_flavor)
     call zero_array(ngpt, nlay, ncol, tau)
     call interpolation( &
       ncol,nlay,this%get_ngas(),nflav,this%get_neta(), & ! dimensions
@@ -527,7 +528,8 @@ contains
 
     !$acc exit data copyout(tau, tau_rayleigh, jtemp, jpress, jeta, tropo, fmajor, fminor)
     !$acc exit data delete(play, tlay, col_gas, col_mix)
-    !$acc exit data delete(this%flavor, this%press_ref_log, this%temp_ref, this%vmr_ref)
+    !$acc exit data delete(this%flavor, this%press_ref_log, this%temp_ref, this%vmr_ref, &
+    !$acc&                 this%gpoint_flavor)
   end function compute_gas_taus
   !------------------------------------------------------------------------------------------
   !

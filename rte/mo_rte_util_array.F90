88e52aed69dd5115cd77d2f9a2a224208c8e0482
Robert Pincus <Robert.Pincus@colorado.edu>
2019-10-22 23:07:30 -0400

    Correcting a file that wasn't merged gracefully.

--- rte/mo_rte_util_array.F90
+++ rte/mo_rte_util_array.F90
@@ -16,20 +16,27 @@ module mo_rte_util_array
 ! These are in a module so code can be written for both CPUs and GPUs
 ! Used only by Fortran classes so routines don't need C bindings and can use assumed-shape
 !
-  use mo_rte_kind,      only: wp
+  use mo_rte_kind,      only: wp, wl
   implicit none
   interface any_vals_less_than
-    module procedure any_vals_less_than_1D, any_vals_less_than_2D, any_vals_less_than_3D
+    module procedure any_vals_less_than_1D,        any_vals_less_than_2D,        any_vals_less_than_3D
+    module procedure any_vals_less_than_1D_masked, any_vals_less_than_2D_masked, any_vals_less_than_3D_masked
   end interface
   interface any_vals_outside
-    module procedure any_vals_outside_1D, any_vals_outside_2D, any_vals_outside_3D
+    module procedure any_vals_outside_1D,        any_vals_outside_2D,        any_vals_outside_3D
+    module procedure any_vals_outside_1D_masked, any_vals_outside_2D_masked, any_vals_outside_3D_masked
   end interface
   interface zero_array
     module procedure zero_array_1D, zero_array_3D, zero_array_4D
   end interface
+  interface extents_are
+    module procedure extents_are_1D, extents_are_2D, extents_are_3D
+    module procedure extents_are_4D, extents_are_5D, extents_are_6D
+    module procedure extents_are_2d_int
+  end interface extents_are
 
   private
-  public :: zero_array, any_vals_less_than, any_vals_outside
+  public :: any_vals_less_than, any_vals_outside, extents_are, zero_array
 contains
   !-------------------------------------------------------------------------------------------------
   ! Values less than a floor
@@ -76,79 +83,221 @@ contains
 
   end function any_vals_less_than_3D
   !-------------------------------------------------------------------------------------------------
+  ! Masked versions
+  !-------------------------------------------------------------------------------------------------
+  logical function any_vals_less_than_1D_masked(array, mask, check_value)
+    real   (wp), dimension(:), intent(in) :: array
+    logical(wl), dimension(:), intent(in) :: mask
+    real   (wp),               intent(in) :: check_value
+
+    real(wp) :: minValue
+
+    !$acc kernels copyin(array)
+    minValue = minval(array, mask=mask)
+    !$acc end kernels
+
+    any_vals_less_than_1D_masked = (minValue < check_value)
+
+  end function any_vals_less_than_1D_masked
+  !-------------------------------------------------------------------------------------------------
+  logical function any_vals_less_than_2D_masked(array, mask, check_value)
+    real   (wp), dimension(:,:), intent(in) :: array
+    logical(wl), dimension(:,:), intent(in) :: mask
+    real   (wp),                 intent(in) :: check_value
+
+    real(wp) :: minValue
+
+    !$acc kernels copyin(array)
+    minValue = minval(array, mask=mask)
+    !$acc end kernels
+
+    any_vals_less_than_2D_masked = (minValue < check_value)
+
+  end function any_vals_less_than_2D_masked
+  !-------------------------------------------------------------------------------------------------
+  logical function any_vals_less_than_3D_masked(array, mask, check_value)
+    real   (wp), dimension(:,:,:), intent(in) :: array
+    logical(wl), dimension(:,:,:), intent(in) :: mask
+    real   (wp),                   intent(in) :: check_value
+
+    real(wp) :: minValue
+
+    !$acc kernels copyin(array)
+    minValue = minval(array, mask=mask)
+    !$acc end kernels
+
+    any_vals_less_than_3D_masked = (minValue < check_value)
+
+  end function any_vals_less_than_3D_masked
+  !-------------------------------------------------------------------------------------------------
   ! Values outside a range
   !-------------------------------------------------------------------------------------------------
-  logical function any_vals_outside_1D(array, minVal, maxVal)
+  logical function any_vals_outside_1D(array, checkMin, checkMax)
     real(wp), dimension(:), intent(in) :: array
-    real(wp),               intent(in) :: minVal, maxVal
+    real(wp),               intent(in) :: checkMin, checkMax
 
-#ifdef _OPENACC
-      ! Compact version using intrinsics below
-      ! but an explicit loop is the only current solution on GPUs
     real(wp) :: minValue, maxValue
-    integer  :: i
-    minValue = minVal
-    maxValue = maxVal
-    !$acc parallel loop copyin(array) reduction(min:minValue) reduction(max:maxValue)
-    do i = 1, size(array,1)
-      minValue = min(array(i), minValue)
-      maxValue = max(array(i), maxValue)
-    end do
-    any_vals_outside_1D = (minValue < minVal .or. maxValue > maxVal)
-#else
-    any_vals_outside_1D = any(array < minVal .or. array > maxVal)
-#endif
+
+    !$acc kernels copyin(array)
+    minValue = minval(array)
+    maxValue = maxval(array)
+    !$acc end kernels
+    any_vals_outside_1D = minValue < checkMin .or. maxValue > checkMax
+
   end function any_vals_outside_1D
 ! ----------------------------------------------------------
-  logical function any_vals_outside_2D(array, minVal, maxVal)
+  logical function any_vals_outside_2D(array, checkMin, checkMax)
     real(wp), dimension(:,:), intent(in) :: array
-    real(wp),                 intent(in) :: minVal, maxVal
+    real(wp),                 intent(in) :: checkMin, checkMax
 
-#ifdef _OPENACC
-      ! Compact version using intrinsics below
-      ! but an explicit loop is the only current solution on GPUs
     real(wp) :: minValue, maxValue
-    integer  :: i, j
-    minValue = minVal
-    maxValue = maxVal
-    !$acc parallel loop collapse(2) copyin(array) reduction(min:minValue) reduction(max:maxValue)
-    do j = 1, size(array,2)
-      do i = 1, size(array,1)
-        minValue = min(array(i,j), minValue)
-        maxValue = max(array(i,j), maxValue)
-      end do
-    end do
-    any_vals_outside_2D = (minValue < minVal .or. maxValue > maxVal)
-#else
-    any_vals_outside_2D = any(array < minVal .or. array > maxVal)
-#endif
+
+    !$acc kernels copyin(array)
+    minValue = minval(array)
+    maxValue = maxval(array)
+    !$acc end kernels
+    any_vals_outside_2D = minValue < checkMin .or. maxValue > checkMax
+
   end function any_vals_outside_2D
 ! ----------------------------------------------------------
-  logical function any_vals_outside_3D(array, minVal, maxVal)
+  logical function any_vals_outside_3D(array, checkMin, checkMax)
     real(wp), dimension(:,:,:), intent(in) :: array
-    real(wp),                   intent(in) :: minVal, maxVal
+    real(wp),                   intent(in) :: checkMin, checkMax
 
-#ifdef _OPENACC
       ! Compact version using intrinsics below
       ! but an explicit loop is the only current solution on GPUs
     real(wp) :: minValue, maxValue
-    integer  :: i, j, k
-    minValue = minVal
-    maxValue = maxVal
-    !$acc parallel loop collapse(3) copyin(array) reduction(min:minValue) reduction(max:maxValue)
-    do k = 1, size(array,3)
-      do j = 1, size(array,2)
-        do i = 1, size(array,1)
-          minValue = min(array(i,j,k), minValue)
-          maxValue = max(array(i,j,k), maxValue)
-        end do
-      end do
-    end do
-    any_vals_outside_3D = (minValue < minVal .or. maxValue > maxVal)
-#else
-    any_vals_outside_3D = any(array < minVal .or. array > maxVal)
-#endif
+
+    !$acc kernels copyin(array)
+    minValue = minval(array)
+    maxValue = maxval(array)
+    !$acc end kernels
+    any_vals_outside_3D = minValue < checkMin .or. maxValue > checkMax
+
   end function any_vals_outside_3D
+  ! ----------------------------------------------------------
+  ! Masked versions
+  ! ----------------------------------------------------------
+  logical function any_vals_outside_1D_masked(array, mask, checkMin, checkMax)
+    real   (wp), dimension(:), intent(in) :: array
+    logical(wl), dimension(:), intent(in) :: mask
+    real(wp),                  intent(in) :: checkMin, checkMax
+
+    real(wp) :: minValue, maxValue
+
+    !$acc kernels copyin(array)
+    minValue = minval(array, mask=mask)
+    maxValue = maxval(array, mask=mask)
+    !$acc end kernels
+    any_vals_outside_1D_masked = minValue < checkMin .or. maxValue > checkMax
+
+  end function any_vals_outside_1D_masked
+! ----------------------------------------------------------
+  logical function any_vals_outside_2D_masked(array, mask, checkMin, checkMax)
+    real   (wp), dimension(:,:), intent(in) :: array
+    logical(wl), dimension(:,:), intent(in) :: mask
+    real(wp),                    intent(in) :: checkMin, checkMax
+
+    real(wp) :: minValue, maxValue
+
+    !$acc kernels copyin(array)
+    minValue = minval(array, mask=mask)
+    maxValue = maxval(array, mask=mask)
+    !$acc end kernels
+    any_vals_outside_2D_masked = minValue < checkMin .or. maxValue > checkMax
+
+  end function any_vals_outside_2D_masked
+! ----------------------------------------------------------
+  logical function any_vals_outside_3D_masked(array, mask, checkMin, checkMax)
+    real   (wp), dimension(:,:,:), intent(in) :: array
+    logical(wl), dimension(:,:,:), intent(in) :: mask
+    real(wp),                      intent(in) :: checkMin, checkMax
+
+    real(wp) :: minValue, maxValue
+
+    !$acc kernels copyin(array)
+    minValue = minval(array, mask=mask)
+    maxValue = maxval(array, mask=mask)
+    !$acc end kernels
+    any_vals_outside_3D_masked = minValue < checkMin .or. maxValue > checkMax
+
+  end function any_vals_outside_3D_masked
+  !--------------------------------------------------------------------------------------------------------------------
+  !
+  ! Extents
+  !
+  ! --------------------------------------------------------------------------------------
+  function extents_are_1d(array, n1)
+    real(wp), dimension(:          ), intent(in) :: array
+    integer,                          intent(in) :: n1
+    logical(wl)                                  :: extents_are_1d
+
+    extents_are_1d = (size(array,1) == n1)
+  end function extents_are_1d
+  ! --------------------------------------------------------------------------------------
+  function extents_are_2d(array, n1, n2)
+    real(wp), dimension(:,:        ), intent(in) :: array
+    integer,                          intent(in) :: n1, n2
+    logical(wl)                                  :: extents_are_2d
+
+    extents_are_2d = (size(array,1) == n1 .and. &
+                      size(array,2) == n2 )
+  end function extents_are_2d
+  ! --------------------------------------------------------------------------------------
+  function extents_are_3d(array, n1, n2, n3)
+    real(wp), dimension(:,:,:      ), intent(in) :: array
+    integer,                          intent(in) :: n1, n2, n3
+    logical(wl)                                  :: extents_are_3d
+
+    extents_are_3d = (size(array,1) == n1 .and. &
+                      size(array,2) == n2 .and. &
+                      size(array,3) == n3)
+  end function extents_are_3d
+  ! --------------------------------------------------------------------------------------
+  function extents_are_4d(array, n1, n2, n3, n4)
+    real(wp), dimension(:,:,:,:    ), intent(in) :: array
+    integer,                          intent(in) :: n1, n2, n3, n4
+    logical(wl)                                  :: extents_are_4d
+
+    extents_are_4d = (size(array,1) == n1 .and. &
+                      size(array,2) == n2 .and. &
+                      size(array,3) == n3 .and. &
+                      size(array,4) == n4)
+  end function extents_are_4d
+  ! --------------------------------------------------------------------------------------
+  function extents_are_5d(array, n1, n2, n3, n4, n5)
+    real(wp), dimension(:,:,:,:,:  ), intent(in) :: array
+    integer,                          intent(in) :: n1, n2, n3, n4, n5
+    logical(wl)                                  :: extents_are_5d
+
+    extents_are_5d = (size(array,1) == n1 .and. &
+                      size(array,2) == n2 .and. &
+                      size(array,3) == n3 .and. &
+                      size(array,4) == n4 .and. &
+                      size(array,5) == n5 )
+  end function extents_are_5d
+  ! --------------------------------------------------------------------------------------
+  function extents_are_6d(array, n1, n2, n3, n4, n5, n6)
+    real(wp), dimension(:,:,:,:,:,:), intent(in) :: array
+    integer,                          intent(in) :: n1, n2, n3, n4, n5, n6
+    logical(wl)                                  :: extents_are_6d
+
+    extents_are_6d = (size(array,1) == n1 .and. &
+                      size(array,2) == n2 .and. &
+                      size(array,3) == n3 .and. &
+                      size(array,4) == n4 .and. &
+                      size(array,5) == n5 .and. &
+                      size(array,6) == n6 )
+  end function extents_are_6d
+  ! --------------------------------------------------------------------------------------
+  function extents_are_2d_int(array, n1, n2)
+    integer,  dimension(:,:        ), intent(in) :: array
+    integer,                          intent(in) :: n1, n2
+    logical(wl)                                  :: extents_are_2d_int
+
+    extents_are_2d_int = (size(array,1) == n1 .and. &
+                          size(array,2) == n2 )
+  end function extents_are_2d_int
   !-------------------------------------------------------------------------------------------------
   ! Initializing arrays to 0
   !-------------------------------------------------------------------------------------------------
@@ -168,9 +317,16 @@ contains
     integer,                         intent(in ) :: ni, nj, nk
     real(wp), dimension(ni, nj, nk), intent(out) :: array
     ! -----------------------
-    !$acc kernels
-    array = 0.0_wp
-    !$acc end kernels
+    integer :: i,j,k
+    ! -----------------------
+    !$acc parallel loop collapse(3) copyout(array)
+    do k = 1, nk
+      do j = 1, nj
+        do i = 1, ni
+          array(i,j,k) = 0.0_wp
+        end do
+      end do
+    end do
 
   end subroutine zero_array_3D
   ! ----------------------------------------------------------
@@ -178,10 +334,19 @@ contains
     integer,                             intent(in ) :: ni, nj, nk, nl
     real(wp), dimension(ni, nj, nk, nl), intent(out) :: array
     ! -----------------------
-    !$acc kernels
-    array = 0.0_wp
-    !$acc end kernels
+    integer :: i,j,k,l
+    ! -----------------------
+    !$acc parallel loop collapse(4) copyout(array)
+    do l = 1, nl
+      do k = 1, nk
+        do j = 1, nj
+          do i = 1, ni
+            array(i,j,k,l) = 0.0_wp
+          end do
+        end do
+      end do
+    end do
 
   end subroutine zero_array_4D
-! ----------------------------------------------------------
+
 end module mo_rte_util_array

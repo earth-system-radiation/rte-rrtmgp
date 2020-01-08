bc490e976bb345324e011fd00a6eaa67ce62768b
Polonsky, Igor <ipolonsky@aer.com>
2020-01-08 13:07:35 -0500

    fixed misplaced  !acc line

--- rrtmgp/mo_gas_optics_rrtmgp.F90
+++ rrtmgp/mo_gas_optics_rrtmgp.F90
@@ -488,6 +488,7 @@ contains
     !
     ! Compute dry air column amounts (number of molecule per cm^2) if user hasn't provided them
     !
+    !$acc enter data create(col_dry_wk, col_dry_arr, col_gas)
     idx_h2o = string_loc_in_array('h2o', this%gas_names)
     if (present(col_dry)) then
       col_dry_wk => col_dry
@@ -495,7 +496,6 @@ contains
       col_dry_arr = get_col_dry(vmr(:,:,idx_h2o), plev) ! dry air column amounts computation
       col_dry_wk => col_dry_arr
     end if
-    !$acc enter data create(col_dry_wk, col_dry_arr, col_gas)
     !
     ! compute column gas amounts [molec/cm^2]
     !

8d333587f34676cca990256fcf9bdbf58f7ab172
Robert Pincus <Robert.Pincus@colorado.edu>
2020-03-17 22:23:55 -0400

    Adding initial regression testing

---
+++ tests/validation-plots.py
@@ -0,0 +1,150 @@
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import seaborn as sb
+import colorcet as cc
+
+def mae(diff, col_dim):
+    #
+    # Mean absolute error
+    #
+    return(xr.ufuncs.fabs(diff).mean(dim=col_dim))
+def rms(diff, col_dim):
+    #
+    # Root mean square error
+    #
+    return(xr.ufuncs.sqrt(xr.ufuncs.square(diff).mean(dim=col_dim)))
+
+def make_comparison_plot(variants, labels, reference, vscale, col_dim="site", lay_dim="layer"):
+    #
+    # Make a plot comparing differences with respect to reference
+    #
+    if type(variants) is not list:
+        make_comparison_plot([variants], [labels], reference, vscale)
+    else:
+        for i in np.arange(0, len(variants)):
+            delta = variants[i] - reference
+            plt.plot(mae(delta, col_dim),
+                     vscale, '-',\
+                     color=cols[i], label=labels[i])
+            plt.plot(rms(delta, col_dim),
+                     vscale, '--',\
+                     color=cols[i])
+        # Reverse vertical ordering
+        plt.legend()
+        # Reverse vertical ordering
+        plt.ylim(vscale.max(), vscale.min())
+
+def construct_lbl_esgf_name(var):
+    #
+    # For a given variable name, provide the OpenDAP URL for the LBLRM line-by-line results
+    #
+    prefix = "http://esgf3.dkrz.de/thredds/dodsC/cmip6/RFMIP/AER/LBLRTM-12-8/rad-irf/r1i1p1f1/Efx/"
+    return(prefix + var + "/gn/v20190514/" + var + "_Efx_LBLRTM-12-8_rad-irf_r1i1p1f1_gn.nc")
+
+########################################################################
+if __name__ == '__main__':
+    gp = xr.open_dataset("test_atmospheres.nc")
+    #
+    # Does the flux plus the Jacobian equal a calculation with perturbed surface temperature?
+    #
+    gp['lw_flux_up_from_deriv'] = gp.lw_flux_up  + gp.lw_jaco_up
+    gp.lw_flux_up_from_deriv.attrs = {"description":"LW flux up, surface T+1K, computed from Jacobian"}
+    lbl = xr.open_mfdataset([construct_lbl_esgf_name(v) for v in ["rsd", "rsu", "rld", "rlu"]],
+                            combine = "by_coords").sel(expt=0)
+    ########################################################################
+    #
+    # The RFMIP cases are on an irregular pressure grid so we can't compute errors
+    #   as a function of pressure
+    # Interpolate onto a uniform pressure level - every 10 hPa
+    #   plev varies from site to site - could the interpolation be made more concise?
+    #
+    plevs = np.arange(0, lbl.plev.max(), 1000)
+    plevs[0] = lbl.plev.min().values
+    plev = xr.DataArray(plevs, dims=['plev'], coords={'plev': plevs})
+    lbli = xr.concat([lbl.sel(site=i).reset_coords().swap_dims({"level":"plev"}).interp(plev=plev) for i in np.arange(0, lbl.site.size)], dim = 'site')
+    gpi  = xr.concat([ gp.sel(site=i).rename({"pres_level":"plev"}).\
+                                      reset_coords().swap_dims({"level":"plev"}).interp(plev=plev) for i in np.arange(0,  gp.site.size)], dim = 'site')
+
+    cols = cc.glasbey_dark
+    with PdfPages('validation-figures.pdf') as pdf:
+        ########################################################################
+        # Longwave
+        ########################################################################
+        #
+        # Accuracy - 3-angle and single-angle
+        #
+        variants = [[gpi.lw_flux_dn, gpi.lw_flux_dn_3ang, gpi.lw_flux_dn_2str],
+                    [gpi.lw_flux_up, gpi.lw_flux_up_3ang, gpi.lw_flux_up_2str],
+                    [gpi.lw_flux_net,
+                    gpi.lw_flux_dn_3ang - gpi.lw_flux_up_3ang]]
+        refs = [lbli.rld,  lbli.rlu,  lbli.rld -  lbli.rlu]
+        titles = ["Accuracy wrt LBLRTM: LW down", "Accuracy wrt LBLRTM: LW up", "Accuracy: LW net"]
+        for v, r, t in zip(variants, refs, titles):
+            make_comparison_plot(v, \
+                                 labels = ["default", "3-angle", "2-stream"], \
+                                 reference = r,            \
+                                 vscale = plev/100.)
+            plt.ylabel("Pressure (Pa)")
+            plt.xlabel("Error (W/m2), solid=mean, dash=RMS")
+            plt.title(t)
+            pdf.savefig()
+            plt.close()
+
+        #
+        # Variants on the default - should be near but not precisely 0
+        #
+        # Level temperatures not provided
+        # Using scattering representations of optical properties
+        #   to do a no-scattering calculation
+        #
+        variants = [[gpi.lw_flux_dn_notlev,  gpi.lw_flux_dn_2str],
+                    [gpi.lw_flux_up_notlev,  gpi.lw_flux_up_2str]]
+        refs =     [gpi.lw_flux_dn,         gpi.lw_flux_up]
+        titles =   ["Variants: LW down",    "Variants: LW up"]
+        for v, r, t in zip(variants, refs, titles):
+            make_comparison_plot(v, \
+                                 labels = ["no-tlev", "2str"], \
+                                 reference = r,            \
+                                 vscale = plev/100.)
+            plt.ylabel("Pressure (Pa)")
+            plt.xlabel("Difference (W/m2), solid=mean, dash=RMS")
+            plt.title(t)
+            pdf.savefig()
+            plt.close()
+
+        variants = [gpi.lw_flux_up_from_deriv]
+        refs =     [gpi.lw_flux_up_stp1]
+        titles =   ["Variants: LW up"]
+        for v, r, t in zip(variants, refs, titles):
+            make_comparison_plot(v, \
+                                 labels = "Jacobian test", \
+                                 reference = r,            \
+                                 vscale = plev/100.)
+            plt.ylabel("Pressure (Pa)")
+            plt.xlabel("Difference (W/m2), solid=mean, dash=RMS")
+            plt.title(t)
+            pdf.savefig()
+            plt.close()
+        ########################################################################
+        # Shortwave
+        #   These are in W/m2 so profiles with high run count for more.
+        #   Would they be better scaled to incoming solar flux?
+        ########################################################################
+        #
+        # Accuracy comparison
+        #
+        variants = [gpi.sw_flux_dn, gpi.sw_flux_up]
+        refs =     [lbli.rsd,               lbli.rsu]
+        titles = ["Accuracy: SW down", "Accuracy: SW up"]
+        for v, r, t in zip(variants, refs, titles):
+            make_comparison_plot(v, \
+                                 labels = "default", \
+                                 reference = r, \
+                                 vscale = plev/100.)
+            plt.ylabel("Pressure (Pa)")
+            plt.xlabel("Error (W/m2), solid=mean, dash=RMS")
+            plt.title(t)
+            pdf.savefig()
+            plt.close()
---
+++ tests/verification.py
@@ -0,0 +1,72 @@
+import xarray as xr
+import argparse
+import sys
+
+def assert_equal(variants, reference):
+    #
+    # Computes difference between reference and each variant, reports if differences
+    #   exceed a threshold
+    #
+    passed = True
+    if type(variants) is not list:
+        assert_equal([variants], reference)
+    else:
+        print('Using %s as reference:'%(reference.description))
+        for v in variants:
+            diff = xr.ufuncs.fabs(v - reference)
+            print('    %s'%(v.description))
+            if diff.max() > report_threshold:
+                print('      differs from reference by as much as %e'%(diff.max()))
+            passed = passed and diff.max() <= failure_threshold
+        print('')
+    return(passed)
+
+
+########################################################################
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Compares all-sky example output to file in reference directory")
+    parser.add_argument("--report_threshold", type=float, default=1.e-10,
+                        help="Threshold for reporting differences")
+    parser.add_argument("--failure_threshold", type=float, default=1.e-5,
+                        help="Threshold at which differences cause failure")
+    args = parser.parse_args()
+    report_threshold  = args.report_threshold
+    failure_threshold = args.failure_threshold
+
+    gp = xr.open_dataset("test_atmospheres.nc")
+    ########################################################################
+    #
+    # Some variants we expect to be essentially equal to the default, so we'll check
+    #   for those
+    ###############################
+    #
+    # Longwave
+    #
+    passed = assert_equal([gp.lw_flux_dn_vr, gp.lw_flux_dn_jaco, gp.lw_flux_dn_subset], gp.lw_flux_dn)
+    passed = assert_equal([gp.lw_flux_up_vr, gp.lw_flux_up_jaco, gp.lw_flux_up_subset], gp.lw_flux_up)  and passed
+    passed = assert_equal(gp.lw_flux_net_2,  gp.lw_flux_net) and passed
+    #
+    # Does the flux plus the Jacobian equal a calculation with perturbed surface temperature?
+    #
+    gp['lw_flux_up_from_deriv'] = gp.lw_flux_up_jaco  + gp.lw_jaco_up
+    gp.lw_flux_up_from_deriv.attrs = {"description":"LW flux up, surface T+1K, computed from Jacobian"}
+    passed = assert_equal(gp.lw_flux_up_from_deriv, gp.lw_flux_up_stp1)  and passed
+    ###############################
+    #
+    # Shortwave
+    #
+    passed = assert_equal([gp.sw_flux_dn_vr, gp.sw_flux_dn_tsi], gp.sw_flux_dn) and passed
+    passed = assert_equal([gp.sw_flux_up_vr, gp.sw_flux_up_tsi], gp.sw_flux_up) and passed
+
+    print('Incrementing')
+    passed = assert_equal([gp.lw_flux_dn_inc_1scl_with_1scl, gp.lw_flux_dn_inc_1scl_with_2str], gp.lw_flux_dn) and passed
+    passed = assert_equal([gp.lw_flux_up_inc_1scl_with_1scl, gp.lw_flux_up_inc_1scl_with_2str], gp.lw_flux_up) and passed
+    # passed = assert_equal(gp.lw_flux_dn_inc_2str_with_1scl,                            gp.lw_flux_dn_2str) and passed
+    # passed = assert_equal(gp.lw_flux_up_inc_2str_with_1scl,                            gp.lw_flux_up_2str) and passed
+    passed = assert_equal([gp.sw_flux_dn_incr],
+                 gp.sw_flux_dn) and passed
+    passed = assert_equal([gp.sw_flux_up_incr],
+                 gp.sw_flux_up) and passed
+
+    if not passed: print("Something is terribly, terribly wrong")
+    sys.exit(0) if passed else sys.exit(1)

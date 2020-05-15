f60f8e0b104c3372835b4fce3690afbd56a3d6ac
Matt Norman <normanmr@ornl.gov>
2020-05-15 17:44:50 -0400

    Adding a CPU machine file for lyra

--- cpp/test/build/cmakescript.sh
+++ cpp/test/build/cmakescript.sh
@@ -5,7 +5,7 @@
 ############################################################################
 ## GET THE NETCDF LINKING FLAGS
 ############################################################################
-NCFLAGS="`$NCHOME/bin/nc-config --libs`"
+NCFLAGS="-L$NCHOME/lib -lnetcdf_c++4 `$NCHOME/bin/nc-config --libs`"
 printf "NetCDF Flags: $NCFLAGS\n\n"
 
 ############################################################################
@@ -15,13 +15,15 @@ CXXFLAGS="$CXXFLAGS -std=c++14 -I$NCHOME/include"
 
 printf "CXXFLAGS: $CXXFLAGS\n\n"
 
-cmake      \
-  -DCMAKE_CXX_FLAGS:STRING="$CXXFLAGS"   \
-  -DNCFLAGS:STRING="$NCFLAGS"            \
-  -DARCH:STRING="$ARCH"                  \
-  -DCUDA_FLAGS:STRING="$CUDA_ARCH"       \
-  -DYAKL_CUB_HOME="$CUBHOME"             \
-  -DYAKL_HOME="$YAKLHOME"                \
+cmake                                  \
+  -DCMAKE_CXX_FLAGS:STRING="$CXXFLAGS" \
+  -DNCFLAGS:STRING="$NCFLAGS"          \
+  -DARCH:STRING="$ARCH"                \
+  -DCUDA_FLAGS:STRING="$CUDA_ARCH"     \
+  -DYAKL_CUB_HOME="$CUBHOME"           \
+  -DYAKL_HIPCUB_HOME="$HIPCUBHOME"     \
+  -DYAKL_ROCPRIM_HOME="$ROCPRIMHOME"   \
+  -DYAKL_HOME="$YAKLHOME"              \
   ..
 
 
---
+++ cpp/test/build/lyra_cpu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source $MODULESHOME/init/bash
+module load gcc hip cmake
+
+unset ARCH
+unset CUDA_ARCH
+unset CUBHOME
+unset ROCPRIMHOME
+unset HIPCUBHOME
+
+export NCHOME=/ccs/home/imn/mysoftware/netcdf-4.3.3.1_gnu
+export CC=gcc
+export CXX=g++
+export CXXFLAGS="-O3"
+export YAKLHOME="/ccs/home/$USER/YAKL"

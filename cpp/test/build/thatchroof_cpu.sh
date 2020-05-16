6d21f40fba04abade5b6ac9bee6bb4fab2b3a6e9
Matt Norman <normanmr@ornl.gov>
2020-05-16 18:31:57 -0400

    Update to thatchroof env files

--- cpp/test/build/thatchroof_cpu.sh
+++ cpp/test/build/thatchroof_cpu.sh
@@ -2,14 +2,14 @@
 
 source $MODULESHOME/init/bash
 module purge
-module load cmake netcdf
+module load cmake
 
 unset ARCH
 unset CUDA_ARCH
 unset CUBHOME
 
-export NCHOME=${NETCDF_PATH}
-export CC=mpicc
-export CXX=mpic++
+export NCFLAGS="`ncxx4-config --libs`"
+export CC=gcc
+export CXX=g++
 export CXXFLAGS="-O3"
 export YAKLHOME="/home/$USER/YAKL"
--- cpp/test/build/thatchroof_gpu.sh
+++ cpp/test/build/thatchroof_gpu.sh
@@ -4,9 +4,9 @@ source $MODULESHOME/init/bash
 module purge
 module load cmake netcdf
 
-export NCHOME=${NETCDF_PATH}
-export CC=mpicc
-export CXX=mpic++
+export NCFLAGS="`ncxx4-config --libs`"
+export CC=gcc
+export CXX=g++
 export CXXFLAGS="-O3"
 export ARCH="CUDA"
 export CUDA_ARCH="-arch sm_35 --std=c++14 --use_fast_math -O3"

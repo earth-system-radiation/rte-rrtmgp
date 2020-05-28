a0f934b031cf5d74e1633dfa88159529a002e51a
Robert Pincus <Robert.Pincus@colorado.edu>
2020-05-27 23:25:05 -0400

    Replace download with OpenDAP access in one comparison script; upload validation plots in Github Actions

--- examples/rfmip-clear-sky/compare-to-reference.py
+++ examples/rfmip-clear-sky/compare-to-reference.py
@@ -12,24 +12,21 @@ import urllib.request
 tst_dir = "."
 rrtmgp_suffix = "_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc"
 
-#
-# Construct URL for RTE+RRTMGP results for RFMIP from ESGF
-#
-def construct_esgf_file(var):
-    esgf_url_base = "http://esgf3.dkrz.de/thredds/fileServer/cmip6/RFMIP/RTE-RRTMGP-Consortium/RTE-RRTMGP-181204/rad-irf/r1i1p1f1/Efx/"
-    # DKRZ node goes down frequently
-    esgf_url_base = "http://esgf-data1.llnl.gov/thredds/fileServer/css03_data/CMIP6/RFMIP/RTE-RRTMGP-Consortium/RTE-RRTMGP-181204/rad-irf/r1i1p1f1/Efx/"
+def construct_esgf_remote_name(var):
+    #
+    # For a given variable name, provide the OpenDAP URL for the RTE+RRTMGP RFMIP results
+    #
+    esgf_url_base = "http://esgf3.dkrz.de/thredds/dodsC/cmip6/RFMIP/RTE-RRTMGP-Consortium/RTE-RRTMGP-181204/rad-irf/r1i1p1f1/Efx/"
+    # DKRZ server has been unstable - better to try the other if one fails
+    esgf_url_base = "http://esgf-data1.llnl.gov/thredds/dodsC/css03_data/CMIP6/RFMIP/RTE-RRTMGP-Consortium/RTE-RRTMGP-181204/rad-irf/r1i1p1f1/Efx/"
     esgf_url_ver  = "gn/v20191007/"
-    return (os.path.join(esgf_url_base, var, esgf_url_ver, var+rrtmgp_suffix))
+    return(os.path.join(esgf_url_base, var, esgf_url_ver, var+rrtmgp_suffix))
+
 #
 # Comparing reference and test results
 #
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Compares all-sky example output to file in reference directory")
-    parser.add_argument("--ref_dir", type=str, default="reference",
-                        help="Directory where reference values are")
-    parser.add_argument("--download_reference", action="store_true",
-                        help="Download reference files even if they exist")
     parser.add_argument("--report_threshold", type=float, default=0.,
                         help="Threshold for reporting differences")
     parser.add_argument("--failure_threshold", type=float, default=1.e-5,
@@ -37,16 +34,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     vars = ['rlu', 'rld', 'rsu', 'rsd']
-    # Download reference data
-    #    If versions of all files exist in the reference directory, no need to download (can be over-ridden)
-    if not all([os.path.exists(os.path.join(args.ref_dir, v + rrtmgp_suffix)) for v in vars]) or args.download_reference:
-        print("Dowloading reference data")
-        os.makedirs(args.ref_dir, exist_ok=True)
-        for v in vars:
-            urllib.request.urlretrieve(construct_esgf_file(v), os.path.join(args.ref_dir, v+rrtmgp_suffix))
-
     tst = xr.open_mfdataset(os.path.join(     tst_dir, "r??" + rrtmgp_suffix), combine='by_coords')
-    ref = xr.open_mfdataset(os.path.join(args.ref_dir, "r??" + rrtmgp_suffix), combine='by_coords')
+    ref = xr.open_mfdataset([construct_esgf_remote_name(v) for v in vars], combine='by_coords')
 
     failed = False
     for v in vars:

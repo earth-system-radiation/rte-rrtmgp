373aeeb0c84e50c78d7f3829812c9e56d3765474
Benjamin R. Hillman <bhillma@sandia.gov>
2020-12-03 15:47:36 -0700

    Add environment for mappy

---
+++ cpp/test/build/mappy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+module purge
+module load sems-env sems-python/3.5.2 sems-gcc/9.2.0 sems-cmake/3.12.2 sems-git/2.10.1 sems-openmpi/4.0.2
+module load sems-netcdf
+export CTEST_PARALLEL_LEVEL=24
+
+unset ARCH
+unset CUDA_ARCH
+unset CUBHOME
+
+export NCFLAGS="`ncxx4-config --libs`"
+export CC=gcc
+export CXX=g++
+export CXXFLAGS="-O3"
+export YAKLHOME="${HOME}/codes/yakl/YAKL"

170132c76077629d09008c1c20827467e3ded172
Benjamin R. Hillman <bhillma@sandia.gov>
2020-12-03 16:26:49 -0700

    Do not do parallel ctests

--- cpp/test/build/mappy.sh
+++ cpp/test/build/mappy.sh
@@ -3,7 +3,7 @@
 module purge
 module load sems-env sems-python/3.5.2 sems-gcc/9.2.0 sems-cmake/3.12.2 sems-git/2.10.1 sems-openmpi/4.0.2
 module load sems-netcdf
-export CTEST_PARALLEL_LEVEL=24
+export CTEST_PARALLEL_LEVEL=1
 
 unset ARCH
 unset CUDA_ARCH

f09b2427320c5bc83c36c45ada352482a260516b
Robert Pincus <Robert.Pincus@colorado.edu>
2021-03-27 23:05:26 -0400

    Don't specify which shell

--- examples/rfmip-clear-sky/stage_files.sh
+++ examples/rfmip-clear-sky/stage_files.sh
@@ -1,5 +1,3 @@
-!#/bin/bash
-
 wget https://psl.noaa.gov/thredds/fileServer/Datasets/rte-rrtmgp/continuous-integration/multiple_input4MIPs_radiation_RFMIP_UColorado-RFMIP-1-2_none.nc
 wget https://psl.noaa.gov/thredds/fileServer/Datasets/rte-rrtmgp/continuous-integration/rld_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc
 wget https://psl.noaa.gov/thredds/fileServer/Datasets/rte-rrtmgp/continuous-integration/rlu_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc

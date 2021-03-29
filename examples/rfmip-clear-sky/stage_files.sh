8a51c139bdbaf70af14056a48fccc71eaa2a56cc
Robert Pincus <Robert.Pincus@colorado.edu>
2021-03-29 15:54:30 -0400

    Get longwave down *and* up in shell

--- examples/rfmip-clear-sky/stage_files.sh
+++ examples/rfmip-clear-sky/stage_files.sh
@@ -2,4 +2,4 @@ wget https://psl.noaa.gov/thredds/fileServer/Datasets/rte-rrtmgp/continuous-inte
 wget https://psl.noaa.gov/thredds/fileServer/Datasets/rte-rrtmgp/continuous-integration/rld_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc
 wget https://psl.noaa.gov/thredds/fileServer/Datasets/rte-rrtmgp/continuous-integration/rlu_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc
 wget https://psl.noaa.gov/thredds/fileServer/Datasets/rte-rrtmgp/continuous-integration/rsd_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc
-wget https://psl.noaa.gov/thredds/fileServer/Datasets/rte-rrtmgp/continuous-integration/rsd_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc
+wget https://psl.noaa.gov/thredds/fileServer/Datasets/rte-rrtmgp/continuous-integration/rsu_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc

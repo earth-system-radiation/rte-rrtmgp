baf36c66189ec2485f6b38c369c4d18cfac77e18
naromero77 <naromero@anl.gov>
2021-05-22 00:36:00 -0500

    CUBHOME needs to be set in the Summit build script.

--- cpp/test/summit_gpu.sh
+++ cpp/test/summit_gpu.sh
@@ -11,4 +11,5 @@ export CXX=g++
 export CXXFLAGS="-O3"
 export ARCH="CUDA"
 export CUDA_ARCH="-arch sm_70 --std=c++14 --use_fast_math -O3"
+export CUBHOME="/ccs/home/$USER/cub"
 export YAKLHOME="/ccs/home/$USER/YAKL"

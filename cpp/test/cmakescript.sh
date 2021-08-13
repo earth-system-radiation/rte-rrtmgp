446b86b024b15854eef09f9d9995e5cde155b675
Benjamin Hillman <bhillma@sandia.gov>
2021-08-13 15:41:06 -0400

    Fixes to set compile flags

--- cpp/test/cmakescript.sh
+++ cpp/test/cmakescript.sh
@@ -8,14 +8,11 @@ printf "NetCDF Flags: $NCFLAGS\n\n"
 printf "NetCDF Include: $NCINCLUDE\n\n"
 printf "CXXFLAGS: $CXXFLAGS\n\n"
 mkdir -p build && cd build
-cmake                                  \
-  -DCMAKE_CXX_FLAGS="$CXXFLAGS"        \
-  -DNCINCLUDE="$NCINCLUDE"             \
-  -DNCFLAGS="$NCFLAGS"                 \
-  -DARCH="$ARCH"                       \
-  -DCUDA_FLAGS="$CUDA_ARCH"            \
-  -DYAKL_HIPCUB_HOME="$HIPCUBHOME"     \
-  -DYAKL_ROCPRIM_HOME="$ROCPRIMHOME"   \
-  -DYAKL_HOME="$YAKLHOME"              \
+cmake                                    \
+  -DCMAKE_CXX_FLAGS="$CXXFLAGS"          \
+  -DNCINCLUDE="$NCINCLUDE"               \
+  -DNCFLAGS="$NCFLAGS"                   \
+  -DYAKL_CUDA_FLAGS="${YAKL_CUDA_FLAGS}" \
+  -DYAKL_ARCH="$ARCH"                    \
+  -DYAKL_HOME="$YAKLHOME"                \
   ..
-

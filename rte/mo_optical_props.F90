06e3b6d7fad5845378f5b95b15d7987ec6b9aa5f
giovanni facco <gfacco@gmail.com>
2021-12-15 13:00:01 -0800

    adding ! for ford formatting

--- rte/mo_optical_props.F90
+++ rte/mo_optical_props.F90
@@ -1,43 +1,43 @@
-!> This code is part of Radiative Transfer for Energetics (RTE)
-!
-! Contacts: Robert Pincus and Eli Mlawer
-! email:  rrtmgp@aer.com
-!
-! Copyright 2015-2018,  Atmospheric and Environmental Research and
-! Regents of the University of Colorado.  All right reserved.
-!
-! Use and duplication is permitted under the terms of the
-!    BSD 3-clause license, see http://opensource.org/licenses/BSD-3-Clause
-! -------------------------------------------------------------------------------------------------
-!
-! Encapsulate optical properties defined on a spectral grid of N bands.
-!   The bands are described by their limiting wavenumbers. They need not be contiguous or complete.
-!   A band may contain more than one spectral sub-point (g-point) in which case a mapping must be supplied.
-!   A name may be provided and will be prepended to error messages.
-!   The base class (ty_optical_props) encapsulates only this spectral discretization and must be initialized
-!      with the spectral information before use.
-!
-!   Optical properties may be represented as arrays with dimensions ncol, nlay, ngpt
-!   (abstract class ty_optical_props_arry).
-!   The type holds arrays depending on how much information is needed
-!   There are three possibilites
-!      ty_optical_props_1scl holds absorption optical depth tau, used in calculations accounting for extinction and emission
-!      ty_optical_props_2str holds extincion optical depth tau, single-scattering albedo ssa, and
-!        asymmetry parameter g. These fields are what's needed for two-stream calculations.
-!      ty_optical_props_nstr holds extincion optical depth tau, single-scattering albedo ssa, and
-!        phase function moments p with leading dimension nmom. These fields are what's needed for multi-stream calculations.
-!   These classes must be allocated before use. Initialization and allocation can be combined.
-!   The classes have a validate() function that checks all arrays for valid values (e.g. tau > 0.)
-!
-! Optical properties can be delta-scaled (though this is currently implemented only for two-stream arrays)
-!
-! Optical properties can increment or "add themselves to" a set of properties represented with arrays
-!   as long as both sets have the same underlying band structure. Properties defined by band
-!   may be added to properties defined by g-point; the same value is assumed for all g-points with each band.
-!
-! Subsets of optical properties held as arrays may be extracted along the column dimension.
-!
-! -------------------------------------------------------------------------------------------------
+!! > This code is part of Radiative Transfer for Energetics (RTE)
+!!
+!! Contacts: Robert Pincus and Eli Mlawer
+!! email:  rrtmgp@aer.com
+!!
+!! Copyright 2015-2018,  Atmospheric and Environmental Research and
+!! Regents of the University of Colorado.  All right reserved.
+!!
+!! Use and duplication is permitted under the terms of the
+!!    BSD 3-clause license, see http://opensource.org/licenses/BSD-3-Clause
+!! -------------------------------------------------------------------------------------------------
+!!
+!! Encapsulate optical properties defined on a spectral grid of N bands.
+!!   The bands are described by their limiting wavenumbers. They need not be contiguous or complete.
+!!   A band may contain more than one spectral sub-point (g-point) in which case a mapping must be supplied.
+!!   A name may be provided and will be prepended to error messages.
+!!   The base class (ty_optical_props) encapsulates only this spectral discretization and must be initialized
+!!      with the spectral information before use.
+!!
+!!   Optical properties may be represented as arrays with dimensions ncol, nlay, ngpt
+!!   (abstract class ty_optical_props_arry).
+!!   The type holds arrays depending on how much information is needed
+!!   There are three possibilites
+!!      ty_optical_props_1scl holds absorption optical depth tau, used in calculations accounting for extinction and emission
+!!      ty_optical_props_2str holds extincion optical depth tau, single-scattering albedo ssa, and
+!!        asymmetry parameter g. These fields are what's needed for two-stream calculations.
+!!      ty_optical_props_nstr holds extincion optical depth tau, single-scattering albedo ssa, and
+!!        phase function moments p with leading dimension nmom. These fields are what's needed for multi-stream calculations.
+!!   These classes must be allocated before use. Initialization and allocation can be combined.
+!!   The classes have a validate() function that checks all arrays for valid values (e.g. tau > 0.)
+!!
+!! Optical properties can be delta-scaled (though this is currently implemented only for two-stream arrays)
+!!
+!! Optical properties can increment or "add themselves to" a set of properties represented with arrays
+!!   as long as both sets have the same underlying band structure. Properties defined by band
+!!   may be added to properties defined by g-point; the same value is assumed for all g-points with each band.
+!!
+!! Subsets of optical properties held as arrays may be extracted along the column dimension.
+!!
+!! -------------------------------------------------------------------------------------------------
 module mo_optical_props
   use mo_rte_kind,              only: wp
   use mo_rte_config,            only: check_extents, check_values

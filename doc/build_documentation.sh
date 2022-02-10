c4159be402ef5fd4c8f9c794bccdd7308918e13f
giovanni facco <gfacco@gmail.com>
2022-02-10 10:18:09 -0800

    fixed folder structure for published site;

--- doc/build_documentation.sh
+++ doc/build_documentation.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+rm -rf ../../public
 ford ford_site/extensions-front-matter.md 
 ford ford_site/kernels-front-matter.md 
 cd jekyll_site/
@@ -7,6 +7,4 @@ bundle exec jekyll build
 cd _site/
 grep -rl --include \*.html /assets/main.css | xargs sed -i 's#/rte-rrtmgp/assets/main.css#assets/main.css#g'
 cd ..
-rm -rf ../../public/jekyll_site 
-mkdir ../../public/jekyll_site
-cp -a _site/. ../../public/jekyll_site
+cp -a _site/. ../../public

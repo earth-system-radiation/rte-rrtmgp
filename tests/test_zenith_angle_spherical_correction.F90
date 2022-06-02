9f6c7edf52212c8db0cfbc791d0edc9ee1e9bd92
Robert Pincus <Robert.Pincus@columbia.edu>
2022-06-01 22:35:39 -0400

    Heating rate calculation uses only diffuse flux in last sunlight (mu0 > 0) layer to avoid spuriously high heating rates.

--- tests/test_zenith_angle_spherical_correction.F90
+++ tests/test_zenith_angle_spherical_correction.F90
@@ -22,9 +22,11 @@ program test_solar_zenith_angle
   use mo_rte_sw,             only: rte_sw
   use mo_load_coefficients,  only: load_and_init
   use mo_heating_rates,      only: compute_heating_rate
+  implicit none
 
-  integer            :: i
-  integer, parameter :: nlay = 80, ncol = 4
+  integer             :: i, icol, ilay, nbnd, ngpt
+  integer,  parameter :: nlay = 80, ncol = 4
+  real(wp), parameter :: geometric_height = 60000._wp ! m
   real(wp), dimension(ncol        ) :: refAlt, refMu
   real(wp), dimension(ncol, 0:nlay) ::    alt,    mu
 
@@ -45,68 +47,86 @@ program test_solar_zenith_angle
   !
   ! Geometric calculation
   !
-  print *, "Geometry "
-  refAlt(:) = nlay * 1000._wp   ! Reference altitude is TOA
+  print *, "Example geometry calculation"
+  refAlt(:) = geometric_height  ! Reference altitude is TOA
   refMu (:) = .02
-  alt =  spread([(i, i = 0, nlay)] * 1000._wp, dim = 1, ncopies=ncol)
+  alt =  spread([(geometric_height/nlay * i, i = 0, nlay)], dim = 1, ncopies=ncol)
   call stop_on_err(zenith_angle_with_height(refAlt, refMu, alt, mu))
-  print *, "alt     mu0"
+  print *, "alt(km)     mu0"
   do i = nlay, 0, -1
-    print *, int(alt(1,i)/1000._wp), mu(1,i)
+    print '(f6.2, 6x, f6.4)', alt(1,i)/1000._wp, mu(1,i)
   end do
 
   !
   ! fluxes
+  !
+  ! Xxample profiles - all identical
   !
   call stop_on_err(make_rcemip_profiles(p_lay(1,:), p_lev(1,:), t_lay(1,:), z_lay(1,:), gas_concs))
   p_lay = spread(p_lay(1,:), dim=1, ncopies=ncol)
   t_lay = spread(t_lay(1,:), dim=1, ncopies=ncol)
   z_lay = spread(z_lay(1,:), dim=1, ncopies=ncol)
   p_lev = spread(p_lev(1,:), dim=1, ncopies=ncol)
-  top_at_1 = p_lay(1, 1) < p_lay(1,nlay)
-  print *, "Top is at 1? ", top_at_1
-
+  !
+  ! Initialize gas optics, optical properties variables
+  !
   call load_and_init(gas_optics, k_dist_file, gas_concs)
-  print *, "temperature limits (K):", gas_optics%get_temp_min(),  gas_optics%get_temp_max()
-
   call stop_on_err(atmos%alloc_2str(ncol, nlay, gas_optics))
   if(gas_optics%source_is_internal()) call stop_on_err("k-distribution file is for the longwave")
   nbnd = gas_optics%get_nband()
   ngpt = gas_optics%get_ngpt()
-
+  !
+  ! Compute gas optics and boundary conditions
+  !
   allocate(toa_flux(ncol, ngpt))
   call stop_on_err(gas_optics%gas_optics(p_lay, p_lev, &
                                          t_lay,        &
                                          gas_concs,    &
                                          atmos,        &
                                          toa_flux))
-
+  !
+  ! Set boundary conditions for fluxes
+  !
   allocate(sfc_alb_dir(nbnd, ncol), sfc_alb_dif(nbnd, ncol))
   sfc_alb_dir = 0._wp
   sfc_alb_dif = 0._wp
   fluxes%flux_up     => flux_up (:,:)
   fluxes%flux_dn     => flux_dn (:,:)
   fluxes%flux_dn_dir => flux_dir(:,:)
-
-  call stop_on_err(zenith_angle_with_height(z_lay(:,1), [0.01_wp, 0.02_wp, 0.03_wp, 0.04_wp], z_lay, mu0))
+  !
+  ! Set small solar zenith angles, varying by column; compute fluxes and heating rates
+  !
+  call stop_on_err(zenith_angle_with_height(z_lay(:,1), 3 * [0.01_wp, 0.02_wp, 0.03_wp, 0.04_wp], z_lay, mu0))
+  top_at_1 = p_lay(1, 1) < p_lay(1,nlay)
   call stop_on_err(rte_sw(atmos, top_at_1, &
                           mu0,   toa_flux, &
                           sfc_alb_dir, sfc_alb_dif, &
                           fluxes))
-  call stop_on_err(compute_heating_rate(flux_up, flux_dn, p_lev, heating_rate))
+  call stop_on_err(compute_heating_rate(flux_up, flux_dn, flux_dir, p_lev, mu0, heating_rate))
 
-  write(*, '("plev (Pa)  play (Pa)    Tlay (K) zlay (m)  flux_dir/dn/up")')
-  write(*, '(f7.0, 33x, 4(5x, f5.0), "  dir")')  p_lev(1,1), flux_dir(:,1)
-  write(*, '(      40x, 4(5x, f5.0), "  dn" )')              flux_dn (:,1)
-  write(*, '(      40x, 4(5x, f5.0), "  up" )')              flux_up (:,1)
+  !
+  ! Text output, top to bottom, levels and layers interleaved
+  !
+  print *, "************************************************************************************"
+  print *, "Flux calculation: ", ncol, " columns"
+  print *, "Pressures and fluxes x3 at levels interleaved with "
+  print *, "    p, t, z, mu0, heating rate on layers"
+  print *
+  print '("plev (Pa)                     flux/heating rate")'
+  print '(f7.0, 33x, 4(5x, f6.2), "  dir")',   p_lev(1,1), flux_dir(:,1)
+  print '(      40x, 4(5x, f6.2), "  dn" )',               flux_dn (:,1)
+  print '(      40x, 4(5x, f6.2), "  up" )',               flux_up (:,1)
   do i = 1, nlay
-    write(*, '(6x, 3(4x, f7.0), 4(4x, f5.2))') p_lay(1,i), t_lay(1,i), z_lay(1,i), &
-                                               heating_rate(:,i) * 86400._wp
-    write(*, '(f7.0, 33x, 4(5x, f5.2), "  dir")')   p_lev(1,i+1), flux_dir(:,i+1)
-    write(*, '(      40x, 4(5x, f5.2), "  dn" )')                 flux_dn (:,i+1)
-    write(*, '(      40x, 4(5x, f5.2), "  up" )')                 flux_up (:,i+1)
+    print '(6x, " p_lay:", f7.0, " t_lay: ", f7.0, " z_lay: ", f7.0)', p_lay(1,i), t_lay(1,i), z_lay(1,i)
+    print '(6x, " mu0:          ", 4(4x, f7.4))', mu0(:,i)
+    print '(6x, " heating rate: ", 4(4x, f7.4))', heating_rate(:,i) * 86400._wp
+    if(i > 1) then
+      if(any(mu0(:, i) > epsilon(mu0) .neqv. mu0(:, i-1) > epsilon(mu0))) &
+      print *, "************ mu0 sign change "
+    end if
+    print '(f7.0, 33x, 4(5x, f6.2), "  dir")',   p_lev(1,i+1), flux_dir(:,i+1)
+    print '(      40x, 4(5x, f6.2), "  dn" )',                 flux_dn (:,i+1)
+    print '(      40x, 4(5x, f6.2), "  up" )',                 flux_up (:,i+1)
   end do
-
-
 
 end program test_solar_zenith_angle

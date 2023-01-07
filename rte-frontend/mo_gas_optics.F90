ea0372c6dccea12a8f0d0fb11d68443b74eb8e35
Robert Pincus <Robert.Pincus@columbia.edu>
2023-01-06 23:12:22 -0500

    More types needed in scope

--- rte-frontend/mo_gas_optics.F90
+++ rte-frontend/mo_gas_optics.F90
@@ -36,7 +36,8 @@ module mo_gas_optics
   use mo_rte_kind,           only: wp
   use mo_source_functions,   only: ty_source_func_lw
   use mo_gas_concentrations, only: ty_gas_concs
-  use mo_optical_props,      only: ty_optical_props, ty_optical_props_arry
+  use mo_optical_props,      only: ty_optical_props, ty_optical_props_arry, & 
+                                   ty_optical_props_1scl, ty_optical_props_2str, ty_optical_props_nstr
 
   public :: combine_abs_and_rayleigh
 
@@ -127,13 +128,13 @@ module mo_gas_optics
     !--------------------------------------------------------------------------------------------------------------------
   end interface
 contains
- !--------------------------------------------------------------------------------------------------------------------
- !
- ! Utility function to combine optical depths from gas absorption and Rayleigh scattering
- !   It may be more efficient to combine scattering and absorption optical depths in place 
- !   rather than storing and processing two large arrays
- !
- subroutine combine_abs_and_rayleigh(tau, tau_rayleigh, optical_props)
+  !--------------------------------------------------------------------------------------------------------------------
+  !
+  ! Utility function to combine optical depths from gas absorption and Rayleigh scattering
+  !   It may be more efficient to combine scattering and absorption optical depths in place 
+  !   rather than storing and processing two large arrays
+  !
+  subroutine combine_abs_and_rayleigh(tau, tau_rayleigh, optical_props)
     real(wp), dimension(:,:,:),   intent(in   ) :: tau
     real(wp), dimension(:,:,:),   intent(in   ) :: tau_rayleigh
     class(ty_optical_props_arry), intent(inout) :: optical_props
@@ -145,76 +146,76 @@ contains
     nlay = size(tau, 2)
     ngpt = size(tau, 3)
     select type(optical_props)
-      type is (ty_optical_props_1scl)
-        !
-        ! Extinction optical depth
-        !
+    type is (ty_optical_props_1scl)
+      !
+      ! Extinction optical depth
+      !
+      !$acc parallel loop gang vector collapse(3) default(present)
+      !$omp target teams distribute parallel do simd collapse(3)
+      do igpt = 1, ngpt
+        do ilay = 1, nlay
+          do icol = 1, ncol
+            optical_props%tau(icol,ilay,igpt) = tau(icol,ilay,igpt) + &
+                                       tau_rayleigh(icol,ilay,igpt)
+          end do
+        end do
+      end do
+    !
+    ! asymmetry factor or phase function moments
+    !
+    type is (ty_optical_props_2str)
+      !
+      ! Extinction optical depth and single scattering albedo
+      !
+      !$acc parallel loop gang vector collapse(3) default(present)
+      !$omp target teams distribute parallel do simd collapse(3)
+      do igpt = 1, ngpt
+        do ilay = 1, nlay
+          do icol = 1, ncol
+            t = tau(icol,ilay,igpt) + tau_rayleigh(icol,ilay,igpt)
+            if(t > 2._wp * tiny(t)) then
+               optical_props%ssa(icol,ilay,igpt) = tau_rayleigh(icol,ilay,igpt) / t
+             else
+               optical_props%ssa(icol,ilay,igpt) = 0._wp
+             end if
+             optical_props%tau(icol,ilay,igpt) = t
+           end do
+        end do
+      end do
+      call zero_array(ncol, nlay, ngpt, optical_props%g)
+    type is (ty_optical_props_nstr)
+      !
+      ! Extinction optical depth and single scattering albedo
+      !
+      !$acc parallel loop gang vector collapse(3) default(present)
+      !$omp target teams distribute parallel do simd collapse(3)
+      do igpt = 1, ngpt
+        do ilay = 1, nlay
+          do icol = 1, ncol
+            t = tau(icol,ilay,igpt) + tau_rayleigh(icol,ilay,igpt)
+            if(t > 2._wp * tiny(t)) then
+               optical_props%ssa(icol,ilay,igpt) = tau_rayleigh(icol,ilay,igpt) / t
+             else
+               optical_props%ssa(icol,ilay,igpt) = 0._wp
+             end if
+             optical_props%tau(icol,ilay,igpt) = t
+           end do
+        end do
+      end do
+      nmom = size(optical_props%p, 1)
+      call zero_array(nmom, ncol, nlay, ngpt, optical_props%p)
+      if(nmom >= 2) then
         !$acc parallel loop gang vector collapse(3) default(present)
         !$omp target teams distribute parallel do simd collapse(3)
         do igpt = 1, ngpt
           do ilay = 1, nlay
             do icol = 1, ncol
-              optical_props%tau(icol,ilay,igpt) = tau(icol,ilay,igpt) + &
-                                         tau_rayleigh(icol,ilay,igpt)
+              optical_props%p(2,icol,ilay,igpt) = 0.1_wp
             end do
           end do
         end do
-      !
-      ! asymmetry factor or phase function moments
-      !
-      type is (ty_optical_props_2str)
-        !
-        ! Extinction optical depth and single scattering albedo
-        !
-        !$acc parallel loop gang vector collapse(3) default(present)
-        !$omp target teams distribute parallel do simd collapse(3)
-        do igpt = 1, ngpt
-          do ilay = 1, nlay
-            do icol = 1, ncol
-              t = tau(icol,ilay,igpt) + tau_rayleigh(icol,ilay,igpt)
-              if(t > 2._wp * tiny(t)) then
-                 optical_props%ssa(icol,ilay,igpt) = tau_rayleigh(icol,ilay,igpt) / t
-               else
-                 optical_props%ssa(icol,ilay,igpt) = 0._wp
-               end if
-               optical_props%tau(icol,ilay,igpt) = t
-             end do
-          end do
-        end do
-        call zero_array(ncol, nlay, ngpt, optical_props%g)
-      type is (ty_optical_props_nstr)
-        !
-        ! Extinction optical depth and single scattering albedo
-        !
-        !$acc parallel loop gang vector collapse(3) default(present)
-        !$omp target teams distribute parallel do simd collapse(3)
-        do igpt = 1, ngpt
-          do ilay = 1, nlay
-            do icol = 1, ncol
-              t = tau(icol,ilay,igpt) + tau_rayleigh(icol,ilay,igpt)
-              if(t > 2._wp * tiny(t)) then
-                 optical_props%ssa(icol,ilay,igpt) = tau_rayleigh(icol,ilay,igpt) / t
-               else
-                 optical_props%ssa(icol,ilay,igpt) = 0._wp
-               end if
-               optical_props%tau(icol,ilay,igpt) = t
-             end do
-          end do
-        end do
-        nmom = size(optical_props%p, 1)
-        call zero_array(nmom, ncol, nlay, ngpt, optical_props%p)
-        if(nmom >= 2) then
-          !$acc parallel loop gang vector collapse(3) default(present)
-          !$omp target teams distribute parallel do simd collapse(3)
-          do igpt = 1, ngpt
-            do ilay = 1, nlay
-              do icol = 1, ncol
-                optical_props%p(2,icol,ilay,igpt) = 0.1_wp
-              end do
-            end do
-          end do
-        end if
-      end select
+      end if
+    end select
   end subroutine combine_abs_and_rayleigh
 
 end module mo_gas_optics

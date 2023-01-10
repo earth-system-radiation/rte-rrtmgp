165de1010b1c04e05c609bcc9e951a3f64a6bc61
Iacono <miacono@pluto.aer.com>
2023-01-10 17:34:44 -0500

    Updated references in aerosol optics extension module

--- extensions/aerosol_optics/mo_aerosol_optics.F90
+++ extensions/aerosol_optics/mo_aerosol_optics.F90
@@ -15,13 +15,14 @@
 !     one sulfate type, and both hydrophobic and hydrophilic black carbon and organic carbon.
 !   Input aerosol optical data are stored in look-up tables.
 !
-!   References for gocart interactive aerosols:                        
-!     Chin et al., 2000 - jgr, v105, 24671-24687                       
-!     Colarco et al., 2010 - jgr, v115, D14207                         
+!   References for the gocart interactive aerosols:   
+!     Chin et al., jgr, 2000 (https://doi.org/10.1029/2000jd900384)
+!     Chin et al., jas, 2002 (https://doi.org/10.1175/1520-0469(2002)059<0461:TAOTFT>2.0.CO;2)
+!     Colarco et al., jgr, 2010 (https://doi.org/10.1029/2009jd012820)
 !                                                                      
 !   References for merra2 aerosol reanalysis:                          
-!     Randles et al., 2017 - jclim, v30, 6823-6850                     
-!     Buchard et al., 2017 - jclim, v30, 6851-6871                     
+!     Randles et al., j. clim., 2017 (https://doi.org/10.1175/jcli-d-16-0609.1) 
+!     Buchard et al., j. clim., 2017 (https://doi.org/10.1175/jcli-d-16-0613.1)
 !                                                                      
 ! The class can be used as-is but is also intended as an example of how to extend the RTE framework
 ! -------------------------------------------------------------------------------------------------

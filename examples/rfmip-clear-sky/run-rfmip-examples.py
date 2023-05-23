442be336b3278120e075643cb9ee94af3deb3a25
Robert Pincus <Robert.Pincus@columbia.edu>
2023-05-23 19:23:16 -0400

    Updating documentation, (unused) Python scripts

--- examples/rfmip-clear-sky/run-rfmip-examples.py
+++ examples/rfmip-clear-sky/run-rfmip-examples.py
@@ -15,11 +15,11 @@ rte_rrtmgp_dir = os.path.join("..", "..")
 rfmip_dir = "."
 # Code should be run in the rfmip_dir directory
 
-conds_file = "multiple_input4MIPs_radiation_RFMIP_UColorado-RFMIP-1-2_none.nc"
-lw_gas_coeffs_file = os.path.join(rte_rrtmgp_dir, "rrtmgp-data",
-                                  "rrtmgp-data-lw-g256-2018-12-04.nc")
-sw_gas_coeffs_file = os.path.join(rte_rrtmgp_dir, "rrtmgp-data",
-                                  "rrtmgp-data-sw-g224-2018-12-04.nc")
+conds_file         = os.path.join(os.environ["RRTMGP_DATA"],
+                   "examples", "rfmip-clear-sky", "inputs",
+                   "multiple_input4MIPs_radiation_RFMIP_UColorado-RFMIP-1-2_none.nc")
+lw_gas_coeffs_file = os.path.join(os.environ["RRTMGP_DATA"], "rrtmgp-gas-lw-g256.nc")
+sw_gas_coeffs_file = os.path.join(os.environ["RRTMGP_DATA"], "rrtmgp-gas-sw-g224.nc")
 
 rfmip_lw_exe_name = "./rrtmgp_rfmip_lw"
 rfmip_sw_exe_name = "./rrtmgp_rfmip_sw"

12e949c42f5dcba6c2531303486413e2c4e9eba9
Robert Pincus <Robert.Pincus@columbia.edu>
2023-07-12 09:08:06 -0400

    Separate program in tests/ to check that equivalent problems produce equivalent results.

--- tests/verification.py
+++ tests/verification.py
@@ -46,6 +46,7 @@ if __name__ == '__main__':
     report_threshold = args.report_threshold
     failure_threshold = args.failure_threshold
 
+    passed = True
     gp = xr.open_dataset("test_atmospheres.nc")
     ########################################################################
     #
@@ -55,20 +56,6 @@ if __name__ == '__main__':
     #
     # Longwave
     #
-    gp['lw_flux_net_from_updn'] = gp.lw_flux_dn - gp.lw_flux_up
-    gp.lw_flux_net_from_updn.attrs = {
-        "description": "LW flux net, computed externally from dn-up"}
-
-    passed = assert_equal(
-        [gp.lw_flux_dn_vr, gp.lw_flux_dn_jaco, gp.lw_flux_dn_subset],
-        gp.lw_flux_dn)
-    passed = (passed and
-              assert_equal(
-                  [gp.lw_flux_up_vr, gp.lw_flux_up_jaco, gp.lw_flux_up_subset],
-                  gp.lw_flux_up))
-    passed = (passed and
-              assert_equal([gp.lw_flux_net, gp.lw_flux_net_2],
-                           gp.lw_flux_net_from_updn))
     #
     # Does the flux plus the Jacobian equal a calculation with perturbed surface
     # temperature?
@@ -78,37 +65,8 @@ if __name__ == '__main__':
         "description": "LW flux up, surface T+1K, computed from Jacobian"}
     passed = (passed and
               assert_equal(gp.lw_flux_up_from_deriv, gp.lw_flux_up_stp1))
-    ###############################
-    #
-    # Shortwave
-    #
-    passed = (passed and
-              assert_equal([gp.sw_flux_dn_vr, gp.sw_flux_dn_tsi],
-                           gp.sw_flux_dn))
-    passed = (passed and
-              assert_equal([gp.sw_flux_up_vr, gp.sw_flux_up_tsi],
-                           gp.sw_flux_up))
 
-    ###############################
-    #
-    # Incrementing
-    #
-    print('Incrementing')
-    passed = assert_equal(
-        [gp.lw_flux_dn_inc_1scl_with_1scl, gp.lw_flux_dn_inc_1scl_with_2str,
-         gp.lw_flux_dn_inc_1scl_with_nstr],
-        gp.lw_flux_dn) and passed
-    passed = assert_equal(
-        [gp.lw_flux_up_inc_1scl_with_1scl, gp.lw_flux_up_inc_1scl_with_2str,
-         gp.lw_flux_up_inc_1scl_with_nstr],
-        gp.lw_flux_up) and passed
-    # passed = assert_equal(gp.lw_flux_dn_inc_2str_with_1scl,
-    #                       gp.lw_flux_dn_2str) and passed
-    # passed = assert_equal(gp.lw_flux_up_inc_2str_with_1scl,
-    #                       gp.lw_flux_up_2str) and passed
-    passed = assert_equal([gp.sw_flux_dn_incr], gp.sw_flux_dn) and passed
-    passed = assert_equal([gp.sw_flux_up_incr], gp.sw_flux_up) and passed
-
+ 
     if not passed:
         print("Something is terribly, terribly wrong")
     sys.exit(0) if passed else sys.exit(1)

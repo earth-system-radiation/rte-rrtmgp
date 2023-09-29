d63ba2d226951a31683d4c8055df3b4e504fdafb
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2023-09-29 19:21:03 +0200

    Fix argument parsing in rfmip-clear-sky drivers

--- examples/rfmip-clear-sky/rrtmgp_rfmip_lw.F90
+++ examples/rfmip-clear-sky/rrtmgp_rfmip_lw.F90
@@ -121,6 +121,7 @@ program rrtmgp_rfmip_lw
   !
   print *, "Usage: rrtmgp_rfmip_lw [block_size] [rfmip_file] [k-distribution_file] [forcing_index (1,2,3)] [physics_index (1,2)]"
   nargs = command_argument_count()
+  if(nargs >= 2) call get_command_argument(2, rfmip_file)
   call read_size(rfmip_file, ncol, nlay, nexp)
   if(nargs >= 1) then
     call get_command_argument(1, block_size_char)
@@ -128,7 +129,6 @@ program rrtmgp_rfmip_lw
   else
     block_size = ncol
   end if
-  if(nargs >= 2) call get_command_argument(2, rfmip_file)
   if(nargs >= 3) call get_command_argument(3, kdist_file)
   if(nargs >= 4) call get_command_argument(4, forcing_index_char)
   if(nargs >= 5) call get_command_argument(5, physics_index_char)

c45e7bc1afe3900f2b3f317b125c8b3b857d5a9e
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2023-09-30 04:33:13 +0200

    Clean up Python scripts (#232)
    * Python: apply PEP8 formatting
    
    * validation-plots.py: fix wrong argument in a wrong place
    
    * validation-plots.py: remove unused argument
    
    * validation-plots.py: add missing argument
    
    * validation-plots.py: fix variable shadowing
    
    * stage_files.py: remove broken branch
    
    * compare-to-reference.py: optimize environment queries

--- examples/rfmip-clear-sky/stage_files.py
+++ examples/rfmip-clear-sky/stage_files.py
@@ -3,8 +3,9 @@
 # This script downloads and/or creates files needed for the RFMIP off-line test
 # cases
 #
-import subprocess
 import sys
+
+import subprocess
 import urllib.request
 from pathlib import Path
 
@@ -39,13 +40,6 @@ Path(conds_file).unlink(missing_ok=True)
 print("Downloading RFMIP input files")
 urllib.request.urlretrieve(conds_url, conds_file)
 
-generate_templates = False
-if generate_templates:
-    print("Downloading scripts for generating output templates")
-    urllib.request.urlretrieve(templ_scr_url, templ_scr)
-    subprocess.run(
-        [sys.executable, templ_scr, "--source_id", "RTE-RRTMGP-181204"])
-else:
-    print("Downloading output templates")
-    for f in output_files:
-        urllib.request.urlretrieve(conds_url.replace(conds_file, f), f)
+print("Downloading output templates")
+for f in output_files:
+    urllib.request.urlretrieve(conds_url.replace(conds_file, f), f)

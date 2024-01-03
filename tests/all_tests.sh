bb69e2e3b1f8ada64665292b27585801d126e775
Robert Pincus <Robert.Pincus@columbia.edu>
2024-01-03 15:46:17 -0500

    Back to shell script, but should fail if any test fails

---
+++ tests/all_tests.sh
@@ -0,0 +1,8 @@
+set -e
+./optical_prop_unit_tests
+./rte_unit_tests
+./check_equivalence test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g128.nc
+./check_equivalence test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g224.nc
+./check_equivalence test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g112.nc
+./check_variants    test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g256.nc ${RRTMGP_DATA}/rrtmgp-gas-lw-g128.nc
+./check_variants    test_atmospheres.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g224.nc ${RRTMGP_DATA}/rrtmgp-gas-sw-g112.nc 

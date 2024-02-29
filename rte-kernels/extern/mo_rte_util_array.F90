b69bb6efb4f08e5832fd1a4f9743404f0fc3a22c
Robert Pincus <Robert.Pincus@columbia.edu>
2024-02-28 21:07:15 -0500

    Syntax

--- rte-kernels/extern/mo_rte_util_array.F90
+++ rte-kernels/extern/mo_rte_util_array.F90
@@ -39,7 +39,7 @@ module mo_rte_util_array
     ! ----------------------------------------------------------
     subroutine zero_array_4D(ni, nj, nk, nl, array) bind(C, name="zero_array_4D")
     use mo_rte_kind,      only: wp, wl
-    integer,                             intent(in ) :: ni, nj, nk, nl
+      integer,                             intent(in ) :: ni, nj, nk, nl
       real(wp), dimension(ni, nj, nk, nl), intent(out) :: array
     end subroutine zero_array_4D
   end interface zero_array

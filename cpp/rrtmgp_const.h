9571a59af22aa32371fb2736d779f7581f9d25a0
James Foucar <jgfouca@sandia.gov>
2024-03-14 16:42:01 -0600

    Remove unused unmanaged stuff

--- cpp/rrtmgp_const.h
+++ cpp/rrtmgp_const.h
@@ -27,32 +27,6 @@ using FOView = Kokkos::Experimental::OffsetView<T, Kokkos::LayoutLeft, Device>;
 
 template <int Rank, typename ExecutionSpace=Kokkos::DefaultExecutionSpace>
 using MDRangeP = Kokkos::MDRangePolicy<ExecutionSpace, Kokkos::Rank<Rank> >;//, Kokkos::Iterate::Right, Kokkos::Iterate::Left> >;
-
-// Copied from EKAT
-template <typename View>
-struct MemoryTraitsMask {
-  enum : unsigned int {
-    value = ((View::traits::memory_traits::is_random_access ? Kokkos::RandomAccess : 0) |
-             (View::traits::memory_traits::is_atomic ? Kokkos::Atomic : 0) |
-             (View::traits::memory_traits::is_restrict ? Kokkos::Restrict : 0) |
-             (View::traits::memory_traits::is_aligned ? Kokkos::Aligned : 0) |
-             (View::traits::memory_traits::is_unmanaged ? Kokkos::Unmanaged : 0))
-      };
-};
-
-// Copied from EKAT
-template <typename View>
-using Unmanaged =
-  // Provide a full View type specification, augmented with Unmanaged.
-  Kokkos::View<typename View::traits::scalar_array_type,
-               typename View::traits::array_layout,
-               typename View::traits::device_type,
-               Kokkos::MemoryTraits<
-                 // All the current values...
-                 MemoryTraitsMask<View>::value |
-                 // ... |ed with the one we want, whether or not it's
-                 // already there.
-                 Kokkos::Unmanaged> >;
 #endif
 
 typedef double real;

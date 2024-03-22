651b04fd5914eba0993b9a7b1f97350ff56512e9
Alex Soklev <alex@makepath.com>
2024-03-22 15:22:59 +0200

    Fix issues with C headers in rrtmgp

--- rte-kernels/api/rte_types.h
+++ rte-kernels/api/rte_types.h
@@ -3,7 +3,7 @@
 Contacts: Robert Pincus and Eli Mlawer
 email:  rrtmgp@aer.com
 
-Copyright 2024-  
+Copyright 2024-
    Trustees of Columbia University in the City of New York
    All right reserved.
 
@@ -11,9 +11,10 @@ Use and duplication is permitted under the terms of the
     BSD 3-clause license, see http://opensource.org/licenses/BSD-3-Clause
 
 This header files C-compatible Boolean and floating point types (see mo_rte_type.F90 for the Fortran equivalent)
-  Adapted from code written by Chiel van Heerwaarden at Wageningen University and Research 
+  Adapted from code written by Chiel van Heerwaarden at Wageningen University and Research
 
 */
+#pragma once
 
 #ifdef RTE_USE_CBOOL
 using Bool = signed char;

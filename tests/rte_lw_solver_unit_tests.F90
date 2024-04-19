f27cf85a000b30fa6d19f1eb4cb7173ed23553e4
Robert Pincus <Robert.Pincus@columbia.edu>
2024-04-19 12:33:23 -0400

    OK so what is the tolerance we need for RE?

--- tests/rte_lw_solver_unit_tests.F90
+++ tests/rte_lw_solver_unit_tests.F90
@@ -317,6 +317,8 @@ contains
     if(.not. allclose(net_flux(:,:), & 
                       spread(net_flux(:,1), dim=2, ncopies=size(net_flux,2)), &
                       tol = 128._wp)) then 
+      print *, maxval((net_flux(:,:) - &
+                       spread(net_flux(:,1), dim=2, ncopies=size(net_flux,2)))/spacing(net_flux(:,:)))
       call report_err("Net flux not constant with tau in gray radiative equilibrium")
       check_gray_rad_equil = .false.
     end if 

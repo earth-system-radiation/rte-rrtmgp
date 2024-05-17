75c2eab1909e8de732d19363525a6f4abea25bcf
Robert Pincus <Robert.Pincus@columbia.edu>
2024-05-17 16:28:45 -0400

    Relaxing thresholds in LW tests for SP

--- tests/rte_lw_solver_unit_tests.F90
+++ tests/rte_lw_solver_unit_tests.F90
@@ -311,7 +311,7 @@ contains
     ! Check top-of-atmosphere energy balance 
     !
     if(.not. allclose(up_flux(:,toa), &
-                      gray_rad_equil_olr(sfc_t, lw_tau), tol=4._wp)) then
+                      gray_rad_equil_olr(sfc_t, lw_tau), tol=8._wp)) then
       call report_err("OLR is not consistent with gray radiative equilibrium")
       check_gray_rad_equil = .false.
     end if 
@@ -322,7 +322,7 @@ contains
     !
     if(.not. allclose(net_flux(:,:), & 
                       spread(net_flux(:,1), dim=2, ncopies=size(net_flux,2)), &
-                      tol = 70._wp)) then 
+                      tol = 100._wp)) then 
       call report_err("Net flux not constant with tau in gray radiative equilibrium")
       check_gray_rad_equil = .false.
     end if 

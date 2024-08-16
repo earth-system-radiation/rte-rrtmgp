1fde63b5c640a95bf0843990a7d6524fc1da2ae4
James Foucar <jgfouca@sandia.gov>
2024-08-16 12:53:42 -0600

    reorder

--- cpp/rrtmgp/kernels/mo_rrtmgp_util_reorder_kernels.h
+++ cpp/rrtmgp/kernels/mo_rrtmgp_util_reorder_kernels.h
@@ -48,22 +48,17 @@ inline void reorder_123x321_kernel(int d1, int d2, int d3, ArrayInT const &array
   using DeviceT = typename ArrayInT::device_type;
   using LayoutT = typename ArrayInT::array_layout;
   using mdrp_t  = typename conv::MDRP<LayoutT, DeviceT>;
-  int constexpr TILE_SIZE = 8;
-  int ntiles1 = (d1-1) / TILE_SIZE + 1;
-  int ntiles3 = (d3-1) / TILE_SIZE + 1;
+  // int constexpr TILE_SIZE = 8;
+  // int ntiles1 = (d1-1) / TILE_SIZE + 1;
+  // int ntiles3 = (d3-1) / TILE_SIZE + 1;
 
   // for (int i2=1; i2<=d2; i2++) {
   //   for (int t3=1; t3<=ntiles3; t3++) {
   //     for (int t1=1; t1<=ntiles1; t1++) {
   //       for (int it3=1; it3<=TILE_SIZE; it3++) {
   //         for (int it1=1; it1<=TILE_SIZE; it1++) {
-  //TIMED_KERNEL(Kokkos::parallel_for( mdrp_t::template get<5>({d2,ntiles1,ntiles3,TILE_SIZE,TILE_SIZE}) , KOKKOS_LAMBDA (int i2, int t1, int t3, int it1, int it3) {
-  //int i3 = t3*TILE_SIZE + it3;
-  //int i1 = t1*TILE_SIZE + it1;
   TIMED_KERNEL(Kokkos::parallel_for( mdrp_t::template get<3>({d1,d2,d3}) , KOKKOS_LAMBDA (int i1, int i2, int i3) {
-    if (i3 < d3 && i1 < d1) {
-      array_out(i3,i2,i1) = array_in(i1,i2,i3);
-    }
+    array_out(i3,i2,i1) = array_in(i1,i2,i3);
   }));
 }
 

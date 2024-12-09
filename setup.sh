e43d38ef1c863ea101bb255d64d5aa0682e942e8
Alexander Soklev <alex@makepath.com>
2024-12-09 17:43:31 +0200

    Removed C bool definition for containerized-ci. Updated C_BOOL as bool option in CMake

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ export FC=gfortran
 export FCFLAGS="-ffree-line-length-none -m64 -std=f2008 -march=native -fbounds-check -fmodule-private -fimplicit-none -finit-real=nan"
 export RRTMGP_DATA_VERSION=v1.8.2
 export FP_MODEL=DP
-export RTE_BOOL=C
+export RTE_CBOOL=ON
 export RTE_KERNELS=default
 export FAILURE_THRESHOLD=7.e-4
 
@@ -15,7 +15,7 @@ cmake -S . -B build -G "Ninja" \
         -DCMAKE_Fortran_FLAGS="$FCFLAGS" \
         -DRRTMGP_DATA_VERSION=$RRTMGP_DATA_VERSION \
         -DPRECISION=$FP_MODEL \
-        -DBOOL_TYPE=$RTE_BOOL \
+        -DUSE_C_BOOL=$RTE_CBOOL \
         -DKERNEL_MODE=$RTE_KERNELS \
         -DENABLE_TESTS=ON \
         -DFAILURE_THRESHOLD=$FAILURE_THRESHOLD \

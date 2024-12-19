1eb449d13540b49180bbc3dbd3121e68c28132c8
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2024-12-19 11:35:34 +0100

    cmake: explicit test handling (run)

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -6,19 +6,6 @@ set(FAILURE_THRESHOLD
 
 message(STATUS "CTest failure threshold: ${FAILURE_THRESHOLD}")
 
-# Function to define a custom test for CTest Arguments: TEST_NAME    - The name
-# of the test in CTest COMMAND_NAME - The command to execute the test
-function(add_custom_test TEST_NAME COMMAND_NAME)
-  # Add the test to CTest with a specific command and working directory
-  add_test(
-    NAME ${TEST_NAME}
-    COMMAND ${COMMAND_NAME} ${ARGN}
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-  )
-
-  message(STATUS "Added test ${TEST_NAME}")
-endfunction()
-
 set(TEST_ATMOSPHERES
     ${RRTMGP_DATA}/examples/rfmip-clear-sky/inputs/multiple_input4MIPs_radiation_RFMIP_UColorado-RFMIP-1-2_none.nc
 )

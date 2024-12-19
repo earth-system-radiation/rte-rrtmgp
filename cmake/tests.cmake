768fade116b239541e95f89a4b1914c49e7411f8
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2024-12-19 11:12:56 +0100

    cmake: explicit test handling (build)

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -6,18 +6,6 @@ set(FAILURE_THRESHOLD
 
 message(STATUS "CTest failure threshold: ${FAILURE_THRESHOLD}")
 
-# Function to define a custom test for CTest Arguments: TEST_NAME - The name of
-# the test in CTest ARGN      - The libraries that are going to be linked
-function(build_test TEST_NAME)
-  set(TEST_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/${TEST_NAME}.F90)
-  set(TEST_LIBS ${ARGN})
-
-  add_executable(${TEST_NAME} ${TEST_SOURCE})
-  target_link_libraries(${TEST_NAME} PRIVATE ${TEST_LIBS})
-
-  message(STATUS "Configuring test ${TEST_NAME}")
-endfunction()
-
 # Function to define a custom test for CTest Arguments: TEST_NAME    - The name
 # of the test in CTest COMMAND_NAME - The command to execute the test
 function(add_custom_test TEST_NAME COMMAND_NAME)

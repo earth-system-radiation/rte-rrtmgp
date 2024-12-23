5336d8b60c177f25e665d1ab830ab64488a1ce0b
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2024-12-23 17:14:18 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,25 +2,22 @@ rm -rf build
 # conda --version
 # conda env create -f environment-dev.yml
 
-export FC=gfortran
-export FCFLAGS="-ffree-line-length-none -m64 -std=f2008 -march=native -fbounds-check -fmodule-private -fimplicit-none -finit-real=nan"
-export RRTMGP_DATA_VERSION=v1.8.2
-export FP_MODEL=DP
-export RTE_CBOOL=ON
-export RTE_KERNELS=default
-export FAILURE_THRESHOLD=7.e-4
+FC=gfortran
+FFLAGS='-ffree-line-length-none -m64 -std=f2008 -march=native -fbounds-check -fmodule-private -fimplicit-none -finit-real=nan'
+RTE_ENABLE_SP=OFF
+KERNEL_MODE=default
+FAILURE_THRESHOLD='7.e-4'
 
 cmake -S . -B build -G "Ninja" \
         -DCMAKE_Fortran_COMPILER=$FC \
-        -DCMAKE_Fortran_FLAGS="$FCFLAGS" \
-        -DRRTMGP_DATA_VERSION=$RRTMGP_DATA_VERSION \
-        -DPRECISION=$FP_MODEL \
-        -DUSE_C_BOOL=$RTE_CBOOL \
-        -DKERNEL_MODE=$RTE_KERNELS \
-        -DENABLE_TESTS=ON \
+        -DCMAKE_Fortran_FLAGS="$FFLAGS" \
+        -DRTE_ENABLE_SP=$RTE_ENABLE_SP \
+        -DKERNEL_MODE=$KERNEL_MODE \
+        -DBUILD_TESTING=ON \
         -DFAILURE_THRESHOLD=$FAILURE_THRESHOLD \
         -DCMAKE_BUILD_TYPE=Release
 
 # cmake --build build --target all --parallel
 
-# ctest --test-dir build/ -V
+# The --test-dir option is available only starting CMake 3.20:
+# ctest -V --test-dir build

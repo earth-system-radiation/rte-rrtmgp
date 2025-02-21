f8ce4bb6f26e03d31965a26f8bd8ab8a03dd1bc2
Alexander Soklev <alex@makepath.com>
2025-02-21 11:37:41 +0200

    Returned the changes to the ci and updated the comment for the tolerances

--- tests/rte_sw_solver_unit_tests.F90
+++ tests/rte_sw_solver_unit_tests.F90
@@ -126,7 +126,8 @@ program rte_sw_solver_unit_tests
     end if
     if(.not. allclose(sfc, &
                       toa_flux(:,1)*mu0_arr*exp(-sum(atmos%tau(:,:,1),dim=2)/mu0_arr), &
-                      tol=24._wp)) then ! Tolerances as big as 20 needed for GPU implementations
+                      tol=24._wp)) then ! Tolerances as big as 24 needed for the optimization the gfortran compiler does
+                                        ! and 20 nedded for GPU implementations
       passed = .false.
       call report_err("Direct flux doesn't match")
     end if

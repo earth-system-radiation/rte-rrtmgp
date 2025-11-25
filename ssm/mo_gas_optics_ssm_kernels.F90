11775786459190d2f8a1074a1e4d5aa5c18041b1
Robert Pincus <Robert.Pincus@columbia.edu>
2025-11-25 18:22:32 -0500

    Content-free template for SSM gas optics kernels

---
+++ ssm/mo_gas_optics_ssm_kernels.F90
@@ -0,0 +1,7 @@
+module mo_gas_optics_ssm_kernels
+  use mo_rte_kind,      only : wp, wl
+  use mo_rte_util_array,only : zero_array
+  implicit none
+  private
+contains
+end module mo_gas_optics_ssm_kernels

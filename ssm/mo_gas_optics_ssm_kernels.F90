2fe408892f3729ce55e67c2b8d0060031c28b628
Robert Pincus <Robert.Pincus@columbia.edu>
2025-11-25 18:23:34 -0500

    pre-commit

--- ssm/mo_gas_optics_ssm_kernels.F90
+++ ssm/mo_gas_optics_ssm_kernels.F90
@@ -1,7 +1,22 @@
+! See documentation in other modules...
+!
+! Contacts: could provide here... names, emails or web site
+!
+! Copyright 2025-,  ... Trustees of Columbia University.  All right reserved.
+!
+! Use and duplication is permitted under the terms of the
+!    BSD 3-clause license, see http://opensource.org/licenses/BSD-3-Clause
+! -------------------------------------------------------------------------------------------------
+!
+!> ## Class implementing Simple Spectral model gas optics
+!>
+!> Details here
+! -------------------------------------------------------------------------------------------------
 module mo_gas_optics_ssm_kernels
   use mo_rte_kind,      only : wp, wl
   use mo_rte_util_array,only : zero_array
   implicit none
   private
+
 contains
 end module mo_gas_optics_ssm_kernels

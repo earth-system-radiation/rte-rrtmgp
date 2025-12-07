7d0d1f6728f30157f24ad1bb2b81919cd0b8c746
Robert Pincus <Robert.Pincus@columbia.edu>
2025-12-07 15:24:10 -0800

    Further details

--- ssm/mo_gas_optics_ssm_kernels.F90
+++ ssm/mo_gas_optics_ssm_kernels.F90
@@ -8,15 +8,55 @@
 !    BSD 3-clause license, see http://opensource.org/licenses/BSD-3-Clause
 ! -------------------------------------------------------------------------------------------------
 !
-!> ## Class implementing Simple Spectral model gas optics
+!> ## Kernels
 !>
 !> Details here
 ! -------------------------------------------------------------------------------------------------
-module mo_gas_optics_ssm_kernels
+module mo_optics_ssm_kernels
   use mo_rte_kind,      only : wp, wl
   use mo_rte_util_array,only : zero_array
   implicit none
   private
-
+  public :: compute_tau, compute_Planck_source
 contains
-end module mo_gas_optics_ssm_kernels
+  !
+  ! Doesn't account for pressure broadening yet...
+  !
+  subroutine compute_tau(ncol, nlay, nnu, ngas,   &
+                         absorption_coeffs, mass, &
+                         tau) bind(C, name="ssm_compute_tau_absorption")
+    integer,  &
+      intent(in ) :: ncol, nlay, nnu, ngas
+    real(wp), dimension(ngases, nnu), &
+      intent(in ) :: absorption_coeffs
+    real(wp), dimension(ngas, ncol, nlay), &
+      intent(in ) :: mass
+    real(wp), dimension(ncol, nlay, nnu), &
+      intent(out) :: tau
+
+    ! Local variables
+    integer :: icol, ilay, inu, igas
+
+
+    do inu = 1, nnu
+      do ilay = 1, nlay
+        do icol = 1, ncol
+          tau(icol, ilay) = &
+            sum([(mass(igas, icol, ilay) * absorption_coeffs(igas, inu), igas = 1, ngas))])
+        end do
+      end do
+    end do
+
+  end subroutine compute_tau
+  ! -------------------------------------------------------------------------------------------------
+  !
+  ! Maybe make an elemental function then call with 1D and 2D wrappers?
+  !
+  subroutine compute_Planck_source(&
+      ncol, nlay, nnu, &
+      nus, dnus, tlay, &
+      source) bind(C, name="ssm_compute_Planck_source")
+
+  end subroutine compute_Planck_source
+
+end module mo_optics_ssm_kernels
--- ssm/mo_ssm_gas_optics.F90
+++ ssm/mo_ssm_gas_optics.F90
@@ -1,6 +1,6 @@
-! See documentation in other modules... 
+! See documentation in other modules...
 !
-! Contacts: could provide here... names, emails or web site 
+! Contacts: could provide here... names, emails or web site
 !
 ! Copyright 2025-,  ... Trustees of Columbia University.  All right reserved.
 !
@@ -10,9 +10,9 @@
 !
 !> ## Class implementing Simple Spectral model gas optics
 !>
-!> Details here 
+!> Details here
 ! -------------------------------------------------------------------------------------------------
-module mo_gas_optics_ssm 
+module mo_optics_ssm
   use mo_rte_kind,           only: wp, wl
   use mo_rte_config,         only: check_extents, check_values
   use mo_rte_util_array,     only: zero_array
@@ -22,48 +22,112 @@ module mo_gas_optics_ssm
   use mo_source_functions,   only: ty_source_func_lw
   use mo_gas_optics_util_string, only: lower_case, string_in_array, string_loc_in_array
   use mo_gas_concentrations, only: ty_gas_concs
-  use mo_optical_props,      only: ty_optical_props_arry, ty_optical_props_1scl, ty_optical_props_2str, ty_optical_props_nstr
+  use mo_optical_props,      only: ty_optical_props_arry, &
+                                   ty_optical_props_1scl, ty_optical_props_2str, ty_optical_props_nstr
   use mo_gas_optics,         only: ty_gas_optics
+  use mo_optics_ssm_kernels, only: compute_tau, compute_Planck_source
+
   implicit none
   private
+
+  real(wp), parameter, public :: Tsun_ssm = 5760._wp ! Default sun temperature for SSM
+  !
+  ! Do the other SSM defaults - absorption parameters, spectral dicretization -
+  !   get declared here as public entities? Or do we add general introscpection?
+  !   (General introspection won't let us recover the triangles)
+  ! If used before configured do we just call this%configure() using defaults?
+  !
   ! -------------------------------------------------------------------------------------------------
-  type, extends(ty_gas_optics), public :: ty_gas_optics_ssm
+  type, extends(ty_gas_optics), public :: ty_optics_ssm
     private
-    ! list of parameters - ideally as small as possible, and ideally extensible 
-    !   maybe? a derived type with a gas name, N slopes/peak locations, 1 continuum? 
-    !
-    ! Need to be able to distinguish LW and SW data
-    ! Need to provide spectral discretization (number of bands and gpoints, which will be the same; starting and ending band wavenumbers)
-    contains 
+    character(32), dimension(:),    allocatable :: gas_names
+    real(wp),      dimension(:, :), allocatable :: absorption_coeffs
+    real(wp),      dimension(:),    allocatable :: nus, dnus
+      ! total absorption coefficients at spectral points (nnus, ngases)
+    real(wp) :: Tstar = 0._wp, &
+                pref  = 100._wp * 100._wp ! 100 hPa
+    contains
       procedure, public :: configure
-      procedure, public :: source_is_internal
-      procedure, public :: source_is_external
       procedure, public :: gas_optics_int
       procedure, public :: gas_optics_ext
+      procedure, public :: source_is_internal
+      procedure, public :: source_is_external
       procedure, public :: get_press_min
       procedure, public :: get_press_max
       procedure, public :: get_temp_min
       procedure, public :: get_temp_max
 
-  end type ty_gas_optics_ssm
+  end type ty_optics_ssm
 
 contains
   !--------------------------------------------------------------------------------------------------------------------
   !
-  !> Configure the simple spectral model parameters 
+  !> Configure the simple spectral model parameters
+  !> Gas optics for simple spectral models are described as N triangles of ln(kappa) for
+  !>   each of M gases.
+  !> Each triangle id defined by
+  !>    a central wavenumber nu0
+  !>    the absorption coefficient at that wavenumber
+  !>    the slope d ln(kappa)/d nu
+  !> By default there are two gases (h2o and co2).
+  !>   h2o has three triangles (rotational lines, and a continuum with infinite slope)
+  !>   co2 has a single triangle
+  !> Absorption coefficents are defined at pref and pressure-broadened as p/pref
+  !> Spectral discretization is defined as
+  !>   max and min wavenumbers and
+  !>   a set of wavenumbers at which to evaluate absorption and Planck function
+  !> If Tstar is provided the gas optics are for insolation
   !
-  function configure(this) result(error_msg)
+  function configure(this,                       &
+                     gas_names, triangle_params, &
+                     nus, nu_min, nu_max,        &
+                     Tstar) result(error_msg)
     !
-    ! All the parameters for the SSM need to get added to the argument list 
+    ! All the parameters for the SSM need to get added to the argument list
     !   At least one of the arguments needs to distinguish LW from SW
-    !   Probably need to specify the spectral discretization during configuration 
-    ! 
-    class(ty_gas_optics_ssm), intent(inout) :: this
+    !   Probably need to specify the spectral discretization during configuration
+    !
+    class(ty_optics_ssm),      intent(inout) :: this
+    character(32), dimension(:),   intent(in   ) :: gas_names
+    real(wp),      dimension(:,:), intent(in   ) :: triangle_params
+      !! (ntriangles, 4) where the second dimension holds [gas_index, kappa0, nu0, l]
+    real(wp),      dimension(:),   intent(in   ) :: nus
+      !! Wavenumbers at which to evaluate Planck function and absorption coefficient
+    real(wp),                      intent(in   ) :: nu_min, nu_max
+      !! Upper and lower bounds of spectrum
+    real(wp),      optional,       intent(in   ) :: Tstar
+      !! Temperature for stellar insolation
     character(len=128)                      :: error_msg     !! Empty if succssful
     ! ----------------------------------------------------------
     ! Local variables
     ! ----------------------------------------------------------
+    integer :: ncol, nlay, nnu, ngas
+
     error_msg = ""
+    ngas = size(gas_names)
+    nnu  = size(nus)
+    ! Input sanitizing?
+    ! triangle params: index <= ngases, kappa0 >= 0; nu_min < nu0s < nu_max; l >= 0
+    ! nus > 0; ascending? nu_min <= nus <= max_nu
+    ! Tstar > 0 if specified
+
+    if(allocated(this%gas_names)) &
+      deallocate(this%gas_names, this%nus, this%dnus, this%absorption_coeffs)
+
+    allocate(this%gas_names(ngas)
+             this%nus (nnu), &
+             this%dnus(nnu), &
+             this%absorption_coeffs(ngas,nnu) )
+    this%nus(1:nnu) = nus(1:nnu)
+
+    ! Spectral discretization - edges of "bands"
+    ! err_message = this%ty_optical_props%init(band_lims_wavenum, band2gpt)
+    ! where band2gpt = [(i, i = 1, nnu)]
+
+    ! Compute absorption coefficients by summing exponentials at each nu
+    ! this%absorption_coeffs = ...
+
+    if(present(Tstar)) this%Tstar = Tstar
   end function configure
   !--------------------------------------------------------------------------------------------------------------------
   !
@@ -75,9 +139,9 @@ contains
                           optical_props, sources,           &
                           col_dry, tlev) result(error_msg)
     ! inputs
-    class(ty_gas_optics_ssm), intent(in   ) :: this
-    real(wp), dimension(:,:), intent(in   ) :: play, &   !! layer pressures [Pa, mb]; (ncol,nlay)
-                                               plev, &   !! level pressures [Pa, mb]; (ncol,nlay+1)
+    class(ty_optics_ssm),     intent(in   ) :: this
+    real(wp), dimension(:,:), intent(in   ) :: play, &   !! layer pressures [Pa]; (ncol,nlay)
+                                               plev, &   !! level pressures [Pa]; (ncol,nlay+1)
                                                tlay      !! layer temperatures [K]; (ncol,nlay)
     real(wp), dimension(:),   intent(in   ) :: tsfc      !! surface skin temperatures [K]; (ncol)
     type(ty_gas_concs),       intent(in   ) :: gas_desc  !! Gas volume mixing ratios
@@ -89,16 +153,51 @@ contains
     character(len=128)                      :: error_msg     !! Empty if succssful
     ! Optional inputs
     real(wp), dimension(:,:),   intent(in   ), &
-                           optional, target :: col_dry, &  !! Column dry amount; dim(ncol,nlay)
+                           optional, target :: col_dry, &  !! Column dry amount; dim(ncol,nlay), will be ignored
                                                tlev        !! level temperatures [K]; (ncol,nlay+1)
     ! ----------------------------------------------------------
     ! Local variables
     ! ----------------------------------------------------------
-    error_msg = ""
-    
-    ! How much data validation to implement? 
 
-    
+
+    error_msg = ""
+
+    ! How much data validation to implement?
+
+    ! Convert pressures and vmr to layer mases (ngas, ncol, nlay)
+
+    !
+    ! Absorption optical depth
+    !   Pressure broadening not yet in place - could just scale mass but that's not pretty
+    !
+    call compute_tau(ncol, nlay, nnu, ngas,   &
+                     this%absorption_coeffs, mass, &
+                     optical_props%tau)
+
+    select type(optical_props)
+      type is (ty_optical_props_2str)
+        call zero_array(ncol, nlay, nnu, optical_props%ssa)
+        call zero_array(ncol, nlay, nnu, optical_props%g)
+      type is (ty_optical_props_nstr)
+        call zero_array(ncol, nlay, nnu, optical_props%ssa)
+        call zero_array(optical_props%get_nmom(), &
+                      ncol, nlay, nnu, optical_props%p)
+    end select
+
+    !
+    ! Planck function sources
+    ! Longwave: sources%sfc_source, sources%lay_source, sources%lev_source, &
+    !            sources%sfc_source_Jac
+    ! How to compute 1D/sfc - another kernel subroutine?
+    ! How to compute Tlev if not provided - make generic?
+    call compute_Planck_source(ncol, nlay,   nnu, &
+                               nus, dnus, tlay,   &
+                               sources%lay_source)
+    call compute_Planck_source(ncol, nlay+1, nnu, &
+                               nus, dnus, tlev,   &
+                               sources%lev_source)
+
+    call zero_array(ncol, nnu, sources%sfc_source_Jac)
   end function gas_optics_int
 
   !------------------------------------------------------------------------------------------
@@ -111,9 +210,9 @@ contains
                           optical_props, toa_src,       & ! mandatory outputs
                           col_dry) result(error_msg)      ! optional input
 
-    class(ty_gas_optics_ssm), intent(in   ) :: this
-    real(wp), dimension(:,:), intent(in   ) :: play, &   !! layer pressures [Pa, mb]; (ncol,nlay)
-                                               plev, &   !! level pressures [Pa, mb]; (ncol,nlay+1)
+    class(ty_optics_ssm),     intent(in   ) :: this
+    real(wp), dimension(:,:), intent(in   ) :: play, &   !! layer pressures [Pa]; (ncol,nlay)
+                                               plev, &   !! level pressures [Pa]; (ncol,nlay+1)
                                                tlay      !! layer temperatures [K]; (ncol,nlay)
     type(ty_gas_concs),       intent(in   ) :: gas_desc  !! Gas volume mixing ratios
     ! output
@@ -140,18 +239,18 @@ contains
   !> return true if initialized for internal sources/longwave, false otherwise
   !
   pure function source_is_internal(this)
-    class(ty_gas_optics_ssm), intent(in) :: this
-    logical                              :: source_is_internal
-    source_is_internal = .true.
+    class(ty_optics_ssm), intent(in) :: this
+    logical                          :: source_is_internal
+    source_is_internal = (Tstar <= 0._wp)
   end function source_is_internal
   !--------------------------------------------------------------------------------------------------------------------
   !
   !> return true if configured for external sources/shortwave, false otherwise
   !
   pure function source_is_external(this)
-    class(ty_gas_optics_ssm), intent(in) :: this
+    class(ty_optics_ssm), intent(in) :: this
     logical                              :: source_is_external
-    source_is_external = .false. 
+    source_is_external = (Tstar > 0._wp)
   end function source_is_external
   !--------------------------------------------------------------------------------------------------------------------
   !
@@ -162,8 +261,8 @@ contains
   !> Minimum valid pressure
   !
   pure function get_press_min(this)
-    class(ty_gas_optics_ssm), intent(in) :: this
-    real(wp)                             :: get_press_min !! minimum valid pressure
+    class(ty_optics_ssm), intent(in) :: this
+    real(wp)                         :: get_press_min !! minimum valid pressure
 
     get_press_min = 0._wp
   end function get_press_min
@@ -173,19 +272,19 @@ contains
   !> Maximum valid pressure
   !
   pure function get_press_max(this)
-    class(ty_gas_optics_ssm), intent(in) :: this
-    real(wp)                                :: get_press_max !! maximum valid pressure
+    class(ty_optics_ssm), intent(in) :: this
+    real(wp)                         :: get_press_max !! maximum valid pressure
 
     get_press_max = huge(1._wp)
   end function get_press_max
 
   !--------------------------------------------------------------------------------------------------------------------
   !
-  !> Minimum valid temperature 
+  !> Minimum valid temperature
   !
   pure function get_temp_min(this)
-    class(ty_gas_optics_ssm), intent(in) :: this
-    real(wp)                             :: get_temp_min !! minimum valid temperature 
+    class(ty_optics_ssm), intent(in) :: this
+    real(wp)                         :: get_temp_min !! minimum valid temperature
     get_temp_min = 0._wp
   end function get_temp_min
 
@@ -194,11 +293,11 @@ contains
   !> Maximum valid pressure
   !
   pure function get_temp_max(this)
-    class(ty_gas_optics_ssm), intent(in) :: this
-    real(wp)                             :: get_temp_max !! maximum valid pressure
+    class(ty_optics_ssm), intent(in) :: this
+    real(wp)                         :: get_temp_max !! maximum valid pressure
 
     get_temp_max = huge(1._wp)
   end function get_temp_max
   !--------------------------------------------------------------------------------------------------------------------
 
-end module mo_gas_optics_ssm
+end module mo_optics_ssm

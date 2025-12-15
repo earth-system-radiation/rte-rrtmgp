04befc768460e29bf3749e9aa1d1a6f8d95e8533
Andrew Williams <anw084@ucsd.edu>
2025-12-15 17:45:07 -0500

    fix trailing and

--- ssm/mo_optics_ssm_kernels.F90
+++ ssm/mo_optics_ssm_kernels.F90
@@ -42,7 +42,7 @@ contains
     real(wp), dimension(ngas, nnu), &
       intent(in ) :: absorption_coeffs
     real(wp), dimension(ncol, nlay), & 
-      intent(in   ) :: play &   !! layer pressures [Pa]; (ncol,nlay)  
+      intent(in   ) :: play    !! layer pressures [Pa]; (ncol,nlay)  
     real(wp), dimension(ngas, ncol, nlay), &
       intent(in ) :: layer_mass
     real(wp), &

bd61f58bf971cc760114ac05b4c4e00cced69982
Robert Pincus <Robert.Pincus@columbia.edu>
2026-03-25 16:41:23 -0400

    Remove default(none) from OpenACC directives

--- rte/frontend/gas-optics-template/mo_gas_concentrations.F90
+++ rte/frontend/gas-optics-template/mo_gas_concentrations.F90
@@ -343,7 +343,7 @@ contains
     !$acc data copyout (array) present(this)
     !$omp target data map(from:array)
     if(size(this%concs(igas)%conc, 2) > 1) then
-      !$acc kernels default(none) present(p)
+      !$acc kernels present(p)
       !$omp target
 #ifdef _CRAYFTN
       array(:) = p(1,:)
@@ -353,7 +353,7 @@ contains
       !$acc end kernels
       !$omp end target
     else
-      !$acc kernels default(none) present(p)
+      !$acc kernels present(p)
       !$omp target
 #ifdef _CRAYFTN
       array(:) = p(1,1)
@@ -403,7 +403,7 @@ contains
     !$acc data copyout (array) present(this, this%concs)
     !$omp target data map(from:array)
     if(size(this%concs(igas)%conc, 1) > 1) then      ! Concentration stored as 2D
-      !$acc parallel loop collapse(2) default(none) present(p)
+      !$acc parallel loop collapse(2) present(p)
       !$omp target teams distribute parallel do simd
       do ilay = 1, size(array,2)
         do icol = 1, size(array,1)
@@ -415,7 +415,7 @@ contains
         end do
       end do
     else if(size(this%concs(igas)%conc, 2) > 1) then ! Concentration stored as 1D
-      !$acc parallel loop collapse(2) default(none) present(p)
+      !$acc parallel loop collapse(2) present(p)
       !$omp target teams distribute parallel do simd
       do ilay = 1, size(array,2)
         do icol = 1, size(array,1)
@@ -427,7 +427,7 @@ contains
         end do
       end do
     else                                             ! Concentration stored as scalar
-      !$acc parallel loop collapse(2) default(none) present(p)
+      !$acc parallel loop collapse(2) present(p)
       !$omp target teams distribute parallel do simd
       do ilay = 1, size(array,2)
         do icol = 1, size(array,1)

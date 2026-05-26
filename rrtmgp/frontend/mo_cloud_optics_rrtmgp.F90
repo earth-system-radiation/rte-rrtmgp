ff15f7c99d560e2ee24e1f2e0cdf55477ab79c95
Max H. Balsmeier <mhbalsmeier@gmail.com>
2026-05-26 23:21:41 +0200

    Effective radius vs. diameter naming fix (#401)
    * naming consistency fix in mo_cloud_optics_rrtmgp
    
    In mo_cloud_optics_rrtmgp, reice has been renamed to diamice, since for ice, the effective diameter is used. Furthermore, one comment and one write statement has also been updated accordingly.
    
    * Further refinements regarding ice effective radius vs. diameter
    
    diamice renamed to dgice in rrtmgp/frontend/mo_cloud_optics_rrtmgp.F90, get_min_radius_ice -> get_min_diameter_ice, get_max_radius_ice -> get_max_diameter_ice, rei renamed to dei and calls of min, max radius functions updated in rrtmgp_allsky.F90
    
    reice renamed to deice in mo_optics_ssm.F90

--- rrtmgp/frontend/mo_cloud_optics_rrtmgp.F90
+++ rrtmgp/frontend/mo_cloud_optics_rrtmgp.F90
@@ -60,9 +60,9 @@ module mo_cloud_optics_rrtmgp
     procedure, public :: finalize
     procedure, public :: cloud_optics
     procedure, public :: get_min_radius_liq
-    procedure, public :: get_min_radius_ice
+    procedure, public :: get_min_diameter_ice
     procedure, public :: get_max_radius_liq
-    procedure, public :: get_max_radius_ice
+    procedure, public :: get_max_diameter_ice
     procedure, public :: get_num_ice_roughness_types
     procedure, public :: set_ice_roughness
   end type ty_cloud_optics_rrtmgp
@@ -209,14 +209,14 @@ contains
   ! Compute single-scattering properties
   !
   function cloud_optics(this,                     &
-                        clwp, ciwp, reliq, reice, &
+                        clwp, ciwp, reliq, dgice, &
                         optical_props) result(error_msg)
     class(ty_cloud_optics_rrtmgp), &
               intent(in   ) :: this
-    real(wp), intent(in   ) :: clwp  (:,:), &   ! cloud liquid water path (g/m2)
-                               ciwp  (:,:), &   ! cloud ice water path    (g/m2)
-                               reliq (:,:), &   ! cloud liquid particle effective size (microns)
-                               reice (:,:)      ! cloud ice particle effective radius  (microns)
+    real(wp), intent(in   ) :: clwp  (:,:), & ! cloud liquid water path (g/m2)
+                               ciwp  (:,:), & ! cloud ice water path    (g/m2)
+                               reliq (:,:), & ! cloud liquid particle effective radius (microns)
+                               dgice (:,:)    ! cloud ice particle effective diameter (microns)
     class(ty_optical_props_arry), &
               intent(inout) :: optical_props
                                                ! Dimensions: (ncol,nlay,ngpt/nbnd)
@@ -261,8 +261,8 @@ contains
         error_msg = "cloud optics: ciwp has wrong extents"
       if(size(reliq, 1) /= ncol .or. size(reliq, 2) /= nlay) &
         error_msg = "cloud optics: reliq has wrong extents"
-      if(size(reice, 1) /= ncol .or. size(reice, 2) /= nlay) &
-        error_msg = "cloud optics: reice has wrong extents"
+      if(size(dgice, 1) /= ncol .or. size(dgice, 2) /= nlay) &
+        error_msg = "cloud optics: dgice has wrong extents"
       if(optical_props%get_ncol() /= ncol .or. optical_props%get_nlay() /= nlay) &
         error_msg = "cloud optics: optical_props have wrong extents"
       if(error_msg /= "") return
@@ -277,10 +277,10 @@ contains
       if(error_msg /= "") return
     end if
 
-    !$acc data copyin(clwp, ciwp, reliq, reice)                         &
+    !$acc data copyin(clwp, ciwp, reliq, dgice)                         &
     !$acc      create(ltau, ltaussa, ltaussag, itau, itaussa, itaussag) &
     !$acc      create(liqmsk,icemsk)
-    !$omp target data map(to:clwp, ciwp, reliq, reice) &
+    !$omp target data map(to:clwp, ciwp, reliq, dgice) &
     !$omp map(alloc:ltau, ltaussa, ltaussag, itau, itaussa, itaussag) &
     !$omp map(alloc:liqmsk, icemsk)
     !
@@ -301,8 +301,8 @@ contains
     if(check_values) then
       if(any_vals_outside(reliq, liqmsk, this%radliq_lwr, this%radliq_upr)) &
         error_msg = 'cloud optics: liquid effective radius is out of bounds'
-      if(any_vals_outside(reice, icemsk, this%diamice_lwr, this%diamice_upr)) &
-        error_msg = 'cloud optics: ice effective radius is out of bounds'
+      if(any_vals_outside(dgice, icemsk, this%diamice_lwr, this%diamice_upr)) &
+        error_msg = 'cloud optics: ice effective diameter is out of bounds'
       if(any_vals_less_than(clwp, liqmsk, 0._wp) .or. any_vals_less_than(ciwp, icemsk, 0._wp)) &
         error_msg = 'cloud optics: negative clwp or ciwp where clouds are supposed to be'
     end if
@@ -332,7 +332,7 @@ contains
         !
         ! Ice
         !
-        call compute_cld_from_table(ncol, nlay, ngpt, icemsk, ciwp, reice,              &
+        call compute_cld_from_table(ncol, nlay, ngpt, icemsk, ciwp, dgice,              &
                                     this%ice_nsteps,this%ice_step_size,this%diamice_lwr,&
                                     this%extice(:,:,this%icergh),                       &
                                     this%ssaice(:,:,this%icergh),                       &
@@ -426,17 +426,17 @@ contains
     r = this%radliq_upr
   end function get_max_radius_liq
   !-----------------------------------------------
-  function get_min_radius_ice(this) result(r)
+  function get_min_diameter_ice(this) result(d)
     class(ty_cloud_optics_rrtmgp), intent(in   ) :: this
-    real(wp)                              :: r
+    real(wp)                              :: d
 
-    r = this%diamice_lwr
-  end function get_min_radius_ice
+    d = this%diamice_lwr
+  end function get_min_diameter_ice
   !-----------------------------------------------
-  function get_max_radius_ice(this) result(r)
+  function get_max_diameter_ice(this) result(d)
     class(ty_cloud_optics_rrtmgp), intent(in   ) :: this
-    real(wp)                              :: r
+    real(wp)                              :: d
 
-    r = this%diamice_upr
-  end function get_max_radius_ice
+    d = this%diamice_upr
+  end function get_max_diameter_ice
 end module mo_cloud_optics_rrtmgp

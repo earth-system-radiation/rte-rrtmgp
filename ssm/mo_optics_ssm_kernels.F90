895131dcbef1b3dcaecd2bf934ae662ba3883beb
Robert Pincus <Robert.Pincus@columbia.edu>
2026-06-19 15:29:43 -0400

    Consolidate SSM kernels

--- ssm/mo_optics_ssm_kernels.F90
+++ ssm/mo_optics_ssm_kernels.F90
@@ -15,22 +15,9 @@
 ! -------------------------------------------------------------------------------------------------
 module mo_optics_ssm_kernels
   use mo_rte_kind,      only : wp, wl
-  use mo_rte_util_array,only : zero_array, set_to_scalar
-  use mo_rte_gas_optics_utils, &
-                        only: grav, compute_Planck_source, compute_layer_mass
-
   implicit none
-
   private
   public :: compute_tau
-
-  !
-  ! Physical constants
-  !
-  real(wp), parameter :: planck_h     = 6.626075540e-34_wp    ! Planck's constant
-  real(wp), parameter :: lightspeed   = 2.99792458e8_wp       ! Speed of light
-  real(wp), parameter :: boltzmann_k  = 1.38065812e-23_wp     ! Boltzman thermodynamic constant
-
 contains
   !
   ! Computes optical depth from atmospheric state and gas optics
@@ -53,38 +40,32 @@ contains
 
     ! Local variables
     integer :: icol, ilay, inu, igas
-    real(wp), dimension(size(play,1), size(play,2)) :: p_scaling
-
-    !$acc        data create(   p_scaling)
-    !$omp target data map(alloc:p_scaling)
 
     ! Apply pressure broadening if pref input is non-zero
     if (pref /= 0._wp) then
-      !$acc                         parallel loop    collapse(2)
-      !$omp target teams distribute parallel do simd collapse(2)
-      do ilay = 1, nlay
-        do icol = 1, ncol
-          p_scaling(icol, ilay) =  play(icol, ilay) / pref
+      !$acc                         parallel loop    collapse(3)
+      !$omp target teams distribute parallel do simd collapse(3)
+      do inu = 1, nnu
+        do ilay = 1, nlay
+          do icol = 1, ncol
+            tau(icol, ilay, inu) = &
+              sum( [(layer_mass(igas, icol, ilay) * absorption_coeffs(igas, inu), igas = 1, ngas)] ) & 
+              * play(icol, ilay) / pref
+          end do
         end do
       end do
     else
-      call set_to_scalar(ncol, nlay, p_scaling, 1._wp)
-    end if
-
-    !$acc                         parallel loop    collapse(3)
-    !$omp target teams distribute parallel do simd collapse(3)
-    do inu = 1, nnu
-      do ilay = 1, nlay
-        do icol = 1, ncol
-          tau(icol, ilay, inu) = &
-            p_scaling(icol, ilay) * sum( [(layer_mass(igas, icol, ilay) * absorption_coeffs(igas, inu), igas = 1, ngas)] )
+      !$acc                         parallel loop    collapse(3)
+      !$omp target teams distribute parallel do simd collapse(3)
+      do inu = 1, nnu
+        do ilay = 1, nlay
+          do icol = 1, ncol
+            tau(icol, ilay, inu) = &
+              sum( [(layer_mass(igas, icol, ilay) * absorption_coeffs(igas, inu), igas = 1, ngas)] )
+          end do
         end do
       end do
-    end do
-
-    !$acc end data
-    !$omp end target data
-
+    end if 
   end subroutine compute_tau
 
 end module mo_optics_ssm_kernels
